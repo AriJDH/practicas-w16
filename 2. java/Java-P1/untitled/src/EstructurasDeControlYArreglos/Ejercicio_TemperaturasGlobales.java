80bc861267512b32c4d03f415f1369a203e18284
David Lozano <david.lozano@mercadolibre.com.co>
2022-07-26 16:52:46 -0500

    Correción de algiritmo temperatura global.

--- 2. java/Java-P1/untitled/src/EstructurasDeControlYArreglos/Ejercicio_TemperaturasGlobales.java
+++ 2. java/Java-P1/untitled/src/EstructurasDeControlYArreglos/Ejercicio_TemperaturasGlobales.java
@@ -19,37 +19,54 @@ public class Ejercicio_TemperaturasGlobales {
 
         //Lenar la información.
         for (int i = 0; i < cantidadCiudades; i++){
-            System.out.println("Digita el nombre de la ciudad #"+ (i+1) +" : ");
-            //nombre de ciudades.
-            nombreCiudades[i] = teclado.nextLine();
+            System.out.println("------------------------------------------------------");
+            System.out.print("Digita el nombre de la ciudad #"+ (i+1) +" : ");
+            nombreCiudades[i] = teclado.next();
+
+            for(int j = 0; j<2;j++){
+                if(j==0){
+                    //Temperatura minima
+                    System.out.print("Digita la temperatura minima de " + nombreCiudades[i] +" : ");
+                    temperaturas[i][j] = teclado.nextInt();
+                    System.out.println();
+                }else{
+                    //Temperatura maxima
+                    System.out.print("Digita la temperatura maxima de " + nombreCiudades[i] +" : ");
+                    temperaturas[i][j] = teclado.nextInt();
+                    System.out.println();
+                }
+            }
         }
 
-        for (int i = 0; i < cantidadCiudades; i++){
-            //Temperatura minima
-            System.out.print("Digita la temperatura minima de " + nombreCiudades[i] +" : ");
-            temperaturas[i][0] = teclado.nextInt();
-            System.out.println("");
-            //Temperatura maxima
-            System.out.print("Digita la temperatura maxima de " + nombreCiudades[i] +" : ");
-            temperaturas[i][1] = teclado.nextInt();
-            System.out.println("");
-        }
-
-
-
-         int index =0;
+        int index =0;
+        int ciudadTemMinima = 0,CiudadTemMax = 0;
         System.out.println("Nombre de ciudades");
 
         for(String nombre: nombreCiudades){
+            System.out.println((index + 1) + ". Ciudad: " + nombre+" la mayor temperatura : "
+                    + temperaturas[index][1]  +"C La menor temperatura es :" + temperaturas[index][0] + "C .");
 
-            System.out.println((index + 1) + ". Ciudad: " + nombre+" la mayor temperatura : " + temperaturas[index][1]  +"C La menor temperatura es :" + temperaturas[index][0] + "C .");
+            //Calculo de la ciudad y temperatura minima y maxima
+            if(index+1 < cantidadCiudades){
+                //Temperatura minima
+                if(temperaturas[index][0] < temperaturas[index+1][0] ){
+                    ciudadTemMinima = index+1;
+                }
+
+                //Temperatura maxima
+                if(temperaturas[index][1] < temperaturas[index+1][1] ){
+                    CiudadTemMax = index+1;
+                }
+            }
+            ++index;
         }
 
-
-
-
-
-
+        System.out.println("-------------------");
+        System.out.println("---INFORME FINAL---");
+        System.out.println("-------------------");
+        System.out.println("La ciudad de " + nombreCiudades[ciudadTemMinima] + " es la ciudad con la temperatura mínima de: " + temperaturas[ciudadTemMinima][0]);
+        System.out.println("La ciudad de " + nombreCiudades[CiudadTemMax] + " es la ciudad con la temperatura máxima de: " + temperaturas[CiudadTemMax][1]);
+        System.out.println("------------------------------------------------------------------------------------------");
 
 
     }
