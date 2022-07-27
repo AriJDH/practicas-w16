59291696f3f57c1e94a4f880caeaa92da9b57322
alerojas-meli <alejandro.rojas@mercadolibre.com>
2022-07-27 17:30:44 -0300

    POO P2

--- 2. java/POO-P2/Ejercicio1/PracticaExcepciones.java
+++ 2. java/POO-P2/Ejercicio1/PracticaExcepciones.java
@@ -8,10 +8,13 @@ public class PracticaExcepciones {
       int b = 300;
 
       try {
-         int result = b/a;
+         if(a==0){
+            throw new IllegalArgumentException("No se puede dividir por cero");
+         }else{
+            int result = b/a;
+         }
       }catch(ArithmeticException e){
-         //System.out.println("Se ha produccion un error");
-         throw new IllegalArgumentException("No se puede dividir por cero");
+         System.out.println("Se ha produccion un error");
       }finally{
          System.out.println("Programa finalizado");
       }
