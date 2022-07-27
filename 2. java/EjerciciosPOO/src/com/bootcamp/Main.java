af08dcf10c166b041e468e04efcecbaa73615c28
KevGamarra <kevin.molano@mercadolibre.com.co>
2022-07-27 09:29:07 -0500

    Init commit

---
+++ 2. java/EjerciciosPOO/src/com/bootcamp/Main.java
@@ -0,0 +1,31 @@
+package com.bootcamp;
+
+public class Main {
+  public static String getIMC(Integer imc){
+    if(imc == -1) return "Bajo peso";
+    if(imc == 0) return "Peso saludable";
+    if(imc == 1) return "Sobrepeso";
+    return "";
+  }
+  public static String isMayor(boolean edad){
+    if(edad) return " Es mayor de edad";
+    return " Es mejor de edad";
+  }
+
+  public static void main(String[] args) {
+    Persona kevin = new Persona();
+    Persona sergio = new Persona("sergio", 28, "1110583037");
+    Persona mary = new Persona("Mary", 32, "1110583039", 61.7,168.1 );
+
+//    Esto no es posible dado que no creamos un constructor que reciba solo estos dos parametros
+//    Persona Marlon = new Persona("Marlon", 25);
+
+    Integer imcMary = mary.calcularIMC();
+    System.out.println("La persona " + mary.getNombre() + " tiene " + getIMC(imcMary));
+
+    boolean esMayor = mary.esMayorDeEdad();
+    System.out.println("La persona " + mary.getNombre() + isMayor(esMayor));
+
+    System.out.println(mary);
+  }
+}
---
+++ 2. java/EjerciciosPOO/src/com/bootcamp/Persona.java
@@ -0,0 +1,73 @@
+package com.bootcamp;
+
+public class Persona {
+  String nombre;
+  Integer edad;
+  String dni;
+  Double peso;
+  Double altura;
+
+  public Persona() {
+  }
+
+  public Persona(String nombre, Integer edad, String dni) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = dni;
+  }
+
+  public Persona(String nombre, Integer edad, String dni, Double peso, Double altura) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = dni;
+    this.peso = peso;
+    this.altura = altura;
+  }
+
+  public Integer calcularIMC() {
+    Double imc = (this.peso / (Math.pow(this.altura, 2)));
+    if (imc < 20) {
+      return -1;
+    } else {
+      if (imc <= 25) {
+        return 0;
+      } else {
+        return -1;
+      }
+    }
+  }
+
+  public boolean esMayorDeEdad() {
+    return this.edad >= 18;
+  }
+
+  @Override
+  public String toString() {
+    return "Soy " + nombre +" y mis datos con los siguientes {" +
+            " edad=" + edad +
+            ", dni='" + dni + '\'' +
+            ", peso=" + peso +
+            ", altura=" + altura +
+            '}';
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public Integer getEdad() {
+    return edad;
+  }
+
+  public String getDni() {
+    return dni;
+  }
+
+  public Double getPeso() {
+    return peso;
+  }
+
+  public Double getAltura() {
+    return altura;
+  }
+}
