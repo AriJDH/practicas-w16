f27f1998951d6d7ce4382afab29d945cf3987ebf
German <german.rrodriguez@mercadolibre.com>
2022-07-27 17:03:48 -0300

    cambio diasParaCaducar a valores que se ajusten al metodo

--- 2. java/PracticaHerenciaPolimorfismo/src/Main/Distribuidora.java
+++ 2. java/PracticaHerenciaPolimorfismo/src/Main/Distribuidora.java
@@ -8,11 +8,11 @@ public class Distribuidora {
 
         ArrayList<Producto> productos = new ArrayList<>();
 
-        productos.add(new Perecedero("producto1",10,10));
-        productos.add(new Perecedero("producto2",23,10));
-        productos.add(new Perecedero("producto3",50,15));
+        productos.add(new Perecedero("producto1",10,3));
+        productos.add(new Perecedero("producto2",23,2));
+        productos.add(new Perecedero("producto3",50,1));
         productos.add(new Perecedero("producto4",10,4));
-        productos.add(new Perecedero("producto5",10,5));
+        productos.add(new Perecedero("producto5",10,0));
 
         productos.add(new NoPerecedero("productoNo1",15,"comida"));
         productos.add(new NoPerecedero("productoNo2",14,"comida"));
