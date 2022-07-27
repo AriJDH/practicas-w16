d36631826f3b9fbfd89da1f33a4ca6e7c10e47ac
mebarboza <melisa.barboza@mercadolibre.com>
2022-07-27 17:58:55 -0300

    Resolucion ejercicio Persona, POO

---
+++ tareasYEjercicios/src/poo/ejercicioPersona/Main.java
@@ -0,0 +1,31 @@
+package poo.ejercicioPersona;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona personaVacia = new Persona();
+        Persona personaConInfoBasica = new Persona("Juan", 30, "12345");
+        Persona personaConTodaLaInfo = new Persona("Pepito", 23, "45678", 60, 180);
+
+        int valorIMC = personaConTodaLaInfo.calcularIMC();
+        switch (valorIMC) {
+            case -1:
+                System.out.println("Bajo peso");
+                break;
+            case 0:
+                System.out.println("Peso saludable");
+                break;
+            case 1:
+                System.out.println("Sobrepeso");
+                break;
+        }
+        if (personaConTodaLaInfo.esMayorDeEdad()) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("No es mayor de edad");
+        }
+        System.out.println(personaConTodaLaInfo);
+    }
+}
+
---
+++ tareasYEjercicios/src/poo/ejercicioPersona/Persona.java
@@ -0,0 +1,57 @@
+package poo.ejercicioPersona;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String dni;
+    private float peso;
+    private float altura;
+
+    public Persona() {
+        nombre = "";
+        edad = 0;
+        dni = "";
+        peso = 0;
+        altura = 0;
+    }
+
+    public Persona(String nombre, int edad, String dni, float peso, float altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public int calcularIMC() {
+        float indice = peso / ((float) Math.pow(altura, 2));
+        if (indice < 20) {
+            return -1;
+        } else if (indice <= 25) {
+            return 0;
+        } else return 1;
+    }
+
+    public boolean esMayorDeEdad() {
+        if (edad >= 18) {
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni='" + dni + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
+    }
+}
