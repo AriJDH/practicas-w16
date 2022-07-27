1a7fa06ec043784e5c48e5996131d2c32be9f587
rnegreyra <rodrigo.negreyra@mercadolibre.com>
2022-07-27 16:35:52 -0300

    Correccion de ejercicio de Productos perecederos y no perecederos

--- 2. java/EjerciciosJava/src/Julio27/Distribuidora.java
+++ 2. java/EjerciciosJava/src/Julio27/Distribuidora.java
@@ -1,21 +1,28 @@
 package Julio27;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Distribuidora {
-    public static Producto[] productos = new Producto[10];
+    public static List<Producto> productos = new ArrayList<Producto>();
     public static void main(String[] args) {
-        productos[0] = new Perecedero("Tomate",10,3);
-        productos[1]  = new Perecedero("lechuga",30,1);
-        productos[2] = new Perecedero("leche",18,2);
-        productos[3]  = new Perecedero("fiambre",50,5);
-        productos[4] = new Perecedero("queso",50,6);
-        productos[5] = new NoPerecedero("arroz",15,"bolza");
+        productos.add(new Perecedero("Tomate",10,3));
+        productos.add(new Perecedero("lechuga",30,1));
+        productos.add(new Perecedero("leche",18,2));
+        productos.add(new Perecedero("fiambre",50,5));
+        productos.add(new Perecedero("queso",50,6));
 
-        productos[6]  = new NoPerecedero("arbejas",20,"enlatado");
-        productos[7]  = new NoPerecedero("lentejas",25,"bolza");
-        productos[8]  = new NoPerecedero("choclo",20,"enlatado");
-        productos[9]  = new NoPerecedero("fideos",19,"bolza");
+        productos.add(new NoPerecedero("arroz",15,"bolza"));
+        productos.add(new NoPerecedero("arbejas",20,"enlatado"));
+        productos.add(new NoPerecedero("lentejas",25,"bolza"));
+        productos.add(new NoPerecedero("choclo",20,"enlatado"));
+        productos.add(new NoPerecedero("fideos",19,"bolza"));
 
-        int precioTotal = 0 ;
-        
+        float precioTotal = 0 ;
+        for (int i =0;i<productos.size();i++)
+        {
+            precioTotal+=productos.get(i).calcular(5);
+        }
+        System.out.println(precioTotal);
     }
 }
