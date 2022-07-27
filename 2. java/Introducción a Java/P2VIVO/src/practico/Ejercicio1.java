c7fc13f5e6df4e6ad3bde5474d4082481769b96e
Guillermo Bariani <guillermo.bariani@mercadolibre.com>
2022-07-27 18:27:54 -0300

    Reformated code

--- 2. java/Introducción a Java/P2VIVO/src/practico/Ejercicio1.java
+++ 2. java/Introducción a Java/P2VIVO/src/practico/Ejercicio1.java
@@ -7,41 +7,41 @@ public class Ejercicio1 {
     HashMap<Integer, List> participantes = new HashMap<Integer, List>();
     HashMap<Integer, String> circuitos = new HashMap<Integer, String>();
 
-    public String elegirCircuito(int edad){
+    public String elegirCircuito(int edad) {
         Scanner entradaCircuito = new Scanner(System.in);
         String circuito = "";
         boolean invalido = false;
         do {
             System.out.println();
             System.out.println("--- Seleccionar Circuito ---");
-            if(edad >= 18){
+            if (edad >= 18) {
                 System.out.println("" +
                         "1) Circuito Chico (2Km por selva y arroyos)\n" +
                         "2) Circuito Medio (5Km por selva, arroyos y barro)\n" +
                         "3) Circuito Avanzado (10km por selva, arroyos, barro y escalada por piedra)");
 
-            }else{
+            } else {
                 System.out.println("" +
                         "1) Circuito Chico (2Km por selva y arroyos)\n" +
                         "2) Circuito Medio (5Km por selva, arroyos y barro)");
             }
             int circuitoElegido = entradaCircuito.nextInt();
 
-            switch (circuitoElegido){
+            switch (circuitoElegido) {
                 case 1:
-                    circuito="Circuito Chico";
+                    circuito = "Circuito Chico";
                     invalido = false;
                     break;
                 case 2:
-                    circuito="Circuito Medio";
+                    circuito = "Circuito Medio";
                     invalido = false;
                     break;
                 case 3:
-                    if(edad < 18){
+                    if (edad < 18) {
                         System.out.println("[ERROR] Ingresó un valor inválido, vuelva a intentar.\n");
                         invalido = true;
-                    }else{
-                        circuito="Circuito Avanzado";
+                    } else {
+                        circuito = "Circuito Avanzado";
                         invalido = false;
                     }
                     break;
@@ -50,25 +50,25 @@ public class Ejercicio1 {
                     invalido = true;
                     break;
             }
-        }while (invalido);
+        } while (invalido);
         return circuito;
     }
 
 
-    public int calcularCostoInscripcion(int edad, String circuito){
+    public int calcularCostoInscripcion(int edad, String circuito) {
         int costo = 0;
-        switch (circuito){
+        switch (circuito) {
             case "Circuito Chico":
-                if (edad < 18){
+                if (edad < 18) {
                     costo = 1300;
-                }else{
+                } else {
                     costo = 1500;
                 }
                 break;
             case "Circuito Medio":
-                if (edad < 18){
+                if (edad < 18) {
                     costo = 2000;
-                }else{
+                } else {
                     costo = 2300;
                 }
                 break;
@@ -79,7 +79,7 @@ public class Ejercicio1 {
         return costo;
     }
 
-    public void inscribirParticipante(){
+    public void inscribirParticipante() {
         List<String> nuevoParticipante = new ArrayList<>();
 
         inscriptosTotales++;
@@ -95,12 +95,12 @@ public class Ejercicio1 {
         dni = entradaInscripcion.nextInt();
         nuevoParticipante.add(1, String.valueOf(dni));
         entradaInscripcion.nextLine();
-        if (participantes.containsKey(dni)){ //Ya está registrado, elegír nueva categoria
+        if (participantes.containsKey(dni)) { //Ya está registrado, elegír nueva categoria
             System.out.print("Ya está registrado el DNI: " + dni + " ");
-            if (circuitos.containsKey(dni)){
+            if (circuitos.containsKey(dni)) {
                 System.out.println("y está inscrito a " + circuitos.get(dni));
 
-            }else{
+            } else {
                 System.out.println();
                 edad = Integer.parseInt(participantes.get(dni).get(4).toString());
                 String circuito = elegirCircuito(edad);
@@ -112,9 +112,9 @@ public class Ejercicio1 {
                 System.out.println();
                 System.out.println("El costo de inscripción es de: $" + costoInscripcion);
             }
-        }else{  //No existe, registrar y elegir categoria.
+        } else {  //No existe, registrar y elegir categoria.
             System.out.print("Nombre: ");
-            nombre =  entradaInscripcion.nextLine();
+            nombre = entradaInscripcion.nextLine();
             nuevoParticipante.add(2, nombre);
             System.out.print("Apellido: ");
             apellido = entradaInscripcion.nextLine();
@@ -144,7 +144,7 @@ public class Ejercicio1 {
         }
     }
 
-    public void mostrarInscriptosPorCircuito(){
+    public void mostrarInscriptosPorCircuito() {
         String circuito = "";
         System.out.println("\n--- Inscritos a un Circuito ---");
         System.out.println("" +
@@ -157,52 +157,52 @@ public class Ejercicio1 {
 
         boolean invalido = false;
         do {
-            switch (circuitoElegido){
+            switch (circuitoElegido) {
                 case 1:
-                    circuito="Circuito Chico";
+                    circuito = "Circuito Chico";
                     break;
                 case 2:
-                    circuito="Circuito Medio";
+                    circuito = "Circuito Medio";
                     break;
                 case 3:
-                    circuito="Circuito Avanzado";
+                    circuito = "Circuito Avanzado";
                     break;
                 default:
                     System.out.println("[ERROR] Ingresó un valor inválido, vuelva a intentar.\n");
                     invalido = true;
                     break;
             }
-        }while (invalido);
+        } while (invalido);
 
-        if (circuitos.containsValue(circuito)){
+        if (circuitos.containsValue(circuito)) {
             System.out.println("\n--- Participantes del " + circuito + " ---");
-            for (Map.Entry inscrito: circuitos.entrySet()){
-                if (inscrito.getValue() == circuito){
+            for (Map.Entry inscrito : circuitos.entrySet()) {
+                if (inscrito.getValue() == circuito) {
                     System.out.println(participantes.get(inscrito.getKey()));
                 }
             }
-        }else{
+        } else {
             System.out.println("\nEl " + circuito + " no tiene participantes inscritos.");
         }
 
     }
 
-    public void desinscribirParticipante(){
+    public void desinscribirParticipante() {
         System.out.println("\n--- Desinscribir participante ---");
         System.out.print("DNI: ");
         Scanner unsubEntrada = new Scanner(System.in);
         int dni = unsubEntrada.nextInt();
 
-        if (circuitos.containsKey(dni)){
+        if (circuitos.containsKey(dni)) {
             String circuitoInscrito = circuitos.get(dni);
             circuitos.remove(dni);
-            System.out.println("Participante desinscrito del "+circuitoInscrito+" con exito!\n");
-        }else {
+            System.out.println("Participante desinscrito del " + circuitoInscrito + " con exito!\n");
+        } else {
             System.out.println("No existe un participante con el DNI: " + dni + "\n");
         }
     }
 
-    public void menu(){
+    public void menu() {
         boolean mantenerSistema = true;
 
         do {
@@ -218,7 +218,7 @@ public class Ejercicio1 {
             Scanner entradaMenu = new Scanner(System.in);
             System.out.print("Opción: ");
             int optMenu = entradaMenu.nextInt();
-            switch (optMenu){
+            switch (optMenu) {
                 case 1:
                     inscribirParticipante();
                     break;
@@ -236,7 +236,7 @@ public class Ejercicio1 {
                     System.out.println("[ERROR] Elegir una opción correcta.");
                     break;
             }
-        }while(mantenerSistema);
+        } while (mantenerSistema);
 
     }
 
