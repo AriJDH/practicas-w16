4911f21666275219ede88686c2f173d493ddda0b
emarivero1 <emanuel.rivero@mercadolibre.com>
2022-07-27 15:11:02 -0300

    add persona

--- 2. java/clase2/src/EjercicioClases/Persona.java
+++ 2. java/clase2/src/EjercicioClases/Persona.java
@@ -9,11 +9,19 @@ public class Persona {
     public Persona(){
 
     }
-    public Persona(String nombre,int edad,String dni){
 
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
     }
-    public Persona(String nombre,int edad,String dni,int peso,int altura){
 
+    public Persona(String nombre, int edad, String dni, int peso, int altura){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso=peso;
+        this.altura=altura;
     }
     public int  calcularIMC(){
         double calcula=peso/(Math.pow(altura,2));
@@ -31,16 +39,25 @@ public class Persona {
         }
         return false;
     }
-    public String toStirng(){
-        return "nombre: "+nombre + "edad: "+edad + "dni: " + dni +  "peso: "+ peso + "altura: " + altura;
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni='" + dni + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
     }
+
 
     public static void main(String[] args) {
     Persona persona=new Persona();
     Persona personaMed=new Persona("emanuel rivero",21,"41704652");
     Persona personaFull=new Persona("Jorge rodolfo",44,"40914753",100,192);
-    System.out.println(personaMed.toStirng());
-    System.out.println(personaFull.toStirng());
+    System.out.println(personaMed);
+    System.out.println(personaFull);
     System.out.println("indice de masa corporal: "+personaFull.calcularIMC());
     System.out.println("es mayor: "+ personaFull.esMayorDeEdad());
 
