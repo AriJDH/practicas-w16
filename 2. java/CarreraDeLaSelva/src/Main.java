f6954f363cdefe65125718cc5bd46674683314f4
Claudio Antolin <claudio.antolin@mercadolibre.cl>
2022-07-29 00:37:36 -0400

    first progress on exercise 'Carrera de la Selva'

---
+++ 2. java/CarreraDeLaSelva/src/Main.java
@@ -0,0 +1,160 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    //Definición de atributos estáticos válidos para toda la clase
+    static List<HashMap> circuitoChico = new ArrayList<>();
+    static List<HashMap> circuitoMedio = new ArrayList<>();
+    static List<HashMap> circuitoAvanzado = new ArrayList<>();
+    static int numeroParticipante = 0;
+
+    public static void iniciarPrograma(boolean programaIniciado){
+        Scanner entrada = new Scanner(System.in);
+        if (!programaIniciado){
+            System.out.println("+++++ Bienvenido al sistema de inscripción de Carrera de la Selva +++++" + "\n"
+                    + "Escoja una de las siguientes opciones: " + "\n"
+                    + "\n"
+                    + "1.- Inscribir a un nuevo participante en una categoría" + "\n"
+                    + "2.- Mostrar a todos los participantes de una categoría" + "\n"
+                    + "3.- Desinscribir a un participante de una categoría" + "\n"
+                    + "4.- Cerrar el programa");
+        }
+        else{
+            System.out.println("Error de ingreso, intente nuevamente: " + "\n"
+                    + "\n"
+                    + "1.- Inscribir a un nuevo participante en una categoría" + "\n"
+                    + "2.- Mostrar a todos los participantes de una categoría" + "\n"
+                    + "3.- Desinscribir a un participante de una categoría" + "\n"
+                    + "4.- Cerrar el programa");
+        }
+
+        if (entrada.nextLine().equals("1")){
+            inscribirParticipante();
+        }
+        else{
+            iniciarPrograma(true);
+        }
+        entrada.close();
+    }
+
+    public static void inscribirParticipante(){
+        HashMap<String, String> participante = new HashMap<>();
+        numeroParticipante += 1;
+        String numParticipante = String.valueOf(numeroParticipante);
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese los datos del participante: " + "\n");
+        System.out.println("DNI del participante: ");
+        participante.put("dni", entrada.nextLine());
+        System.out.println("Nombre del participante: ");
+        participante.put("nombre", entrada.nextLine());
+        System.out.println("Apellido del participante: ");
+        participante.put("apellido", entrada.nextLine());
+        System.out.println("Edad del participante: ");
+        participante.put("edad", entrada.nextLine());
+        /*System.out.println("Celular del participante: ");
+        participante.put("celular", entrada.nextLine());
+        System.out.println("Número de emergencia del participante: ");
+        participante.put("numEmergencia", entrada.nextLine());
+        System.out.println("Grupo sanguíneo del participante: ");
+        participante.put("grupoSanguineo", entrada.nextLine());*/
+
+        System.out.println("Ingrese el circuito de competición del participante: " + "\n"
+        + "1.- Circuito chico" + "\n"
+        + "2.- Circuito medio" + "\n"
+        + "3.- Circuito avanzado");
+
+        switch (entrada.nextLine()){
+            case "1":
+                if (Integer.parseInt(participante.get("edad")) < 18){
+                participante.put("abono", "1300");
+                }
+                else{
+                participante.put("abono", "1500");
+                }
+                participante.put("numeroParticipante", numParticipante);
+                circuitoChico.add(participante);
+            break;
+            case "2":
+                if (Integer.parseInt(participante.get("edad")) < 18){
+                    participante.put("abono", "2000");
+                }
+                else{
+                    participante.put("abono", "2300");
+                }
+                participante.put("numeroParticipante", numParticipante);
+                circuitoMedio.add(participante);
+                break;
+            case "3":
+                if (Integer.parseInt(participante.get("edad")) < 18){
+                    System.out.println("No se permite la inscripción de menores de edad en esta categoría");
+                    iniciarPrograma(true);
+                }
+                else{
+                    participante.put("abono", "2800");
+                }
+                participante.put("numeroParticipante", numParticipante);
+                circuitoAvanzado.add(participante);
+                break;
+            default:
+                System.out.println("Ingreso no permitido");
+                break;
+        }
+
+        if (!circuitoChico.isEmpty()){
+            for (int i = 0; i < circuitoChico.size(); i++){
+            System.out.println(circuitoChico.get(i).values());
+        }
+        }
+        if (!circuitoMedio.isEmpty()){
+            for (int i = 0; i < circuitoMedio.size(); i++){
+                System.out.println(circuitoMedio.get(i).values());
+            }
+        }
+        if (!circuitoAvanzado.isEmpty()){
+            for (int i = 0; i < circuitoAvanzado.size(); i++){
+                System.out.println(circuitoAvanzado.get(i).values());
+            }
+        }
+
+
+        System.out.println("¿Desea agregar a otro participante? (y/n)");
+        String input = entrada.nextLine();
+        boolean entrarAWhile = false;
+
+        if (input.equals("y")){
+            inscribirParticipante();
+            entrada.close();
+        }
+        else if (input.equals("n")){
+            iniciarPrograma(false);
+            entrada.close();
+        }
+        else{
+            entrarAWhile = true;
+        }
+
+        while(entrarAWhile){
+            System.out.println("Ingreso no permitido, intente nuevamente (y/n)");
+            String teclado = entrada.nextLine();
+            if (teclado.equals("y")){
+                entrarAWhile = false;
+                inscribirParticipante();
+                entrada.close();
+            }
+            if (teclado.equals("n")){
+                entrarAWhile = false;
+                iniciarPrograma(false);
+                entrada.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        iniciarPrograma(false);
+
+    }
+}
--- 2. java/readme.md
+++ 2. java/readme.md
@@ -1,3 +1,4 @@
 - 26-07-2022: se sube la resolución del ejercicio "Estructuras de control y Arreglos" como proyecto de IntelliJ Idea en el archivo "estructuras_arreglos".
 - 28-07-2022: se sube la resolución de los ejercicios de la unidad de introducción a Java, referidos a: clases y objetos, excepciones y herencia, polimorfismo y encapsulamiento, en el archivo "Introduccion_Java".
 - 28-07-2022: se sube la resolución del ejercicio 1 de la unidad Clases Abstractas e Interfaces, en el archivo "AbsClass_Interface"
+- 29-07-2022: se sube el primer avance de la resolución del ejercicio de estructuras dinámicas en el archivo "CarreraDeLaSelva"
