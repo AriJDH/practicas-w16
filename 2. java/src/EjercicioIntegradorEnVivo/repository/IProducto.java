f24aca3608b65f82d98cbcd22955afb0e7df5a36
DrigoMELI <rodrigo.cifuentes@mercadolibre.cl>
2022-07-29 15:56:12 -0400

    [ADD] descuentos de viaje.

---
+++ 2. java/src/EjercicioIntegradorEnVivo/repository/IProducto.java
@@ -0,0 +1,8 @@
+package EjercicioIntegradorEnVivo.repository;
+
+import EjercicioIntegradorEnVivo.models.Producto;
+
+public interface IProducto {
+
+    public Producto crearProducto(Producto reserva);
+}
