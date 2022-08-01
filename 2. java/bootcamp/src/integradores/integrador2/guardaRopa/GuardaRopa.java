aebdbaf0d5f4c578584426062af72db09c6f41a8
Sofia Petrides <sofia.petrides@mercadolibre.com>
2022-08-01 12:46:34 -0300

    guardaropa

---
+++ 2. java/bootcamp/src/integradores/integrador2/guardaRopa/GuardaRopa.java
@@ -0,0 +1,68 @@
+package integradores.integrador2.guardaRopa;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GuardaRopa {
+
+
+    private Map<Integer, List<Prenda>> diccionario;
+    private int contador;
+
+
+    public Integer guardarPrendas(List<Prenda> prendas) {
+
+        contador++;
+        diccionario.put(contador, prendas);
+        return contador;
+
+    }
+
+
+    public GuardaRopa() {
+
+        diccionario = new HashMap<>();
+
+    }
+
+    public void mostrarPrendas() {
+
+        diccionario.forEach((id, prenda) -> System.out.println(id + "-" + prenda.toString()));
+
+    }
+
+    public List<Prenda> devolverPrendas(Integer numero) {
+
+        if (diccionario.containsKey(numero)) {
+            return diccionario.get(numero);
+
+        }
+        return null;
+    }
+
+    public Map<Integer, List<Prenda>> getDiccionario() {
+        return diccionario;
+    }
+
+    public void setDiccionario(Map<Integer, List<Prenda>> diccionario) {
+        this.diccionario = diccionario;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
+    @Override
+    public String toString() {
+        return "GuardaRopa{" +
+                "diccionario=" + diccionario +
+                ", contador=" + contador +
+                '}';
+    }
+}
---
+++ 2. java/bootcamp/src/integradores/integrador2/guardaRopa/Main.java
@@ -0,0 +1,28 @@
+package integradores.integrador2.guardaRopa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Prenda uno = new Prenda("nike","pantalon");
+        Prenda dos = new Prenda("nike" , "pantalon2");
+
+        List<Prenda> prendas = new ArrayList<>();
+        prendas.add(uno);
+        prendas.add(dos);
+
+
+        GuardaRopa guardaRopa = new GuardaRopa();
+        int id = guardaRopa.guardarPrendas(prendas);
+        guardaRopa.mostrarPrendas();
+        guardaRopa.devolverPrendas(id).stream().forEach(System.out::println);
+
+
+
+
+    }
+}
