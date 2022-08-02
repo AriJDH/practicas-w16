b4db704437a03939d034d67c9a7ee7841e67aa5a
Sofia Petrides <sofia.petrides@mercadolibre.com>
2022-08-02 13:29:55 -0300

    spring roman numbers

---
+++ 3. spring/romannumbers/src/main/java/com/bootcamp/romannumbers/RomannumbersApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.romannumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomannumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomannumbersApplication.class, args);
+	}
+
+}
---
+++ 3. spring/romannumbers/src/test/java/com/bootcamp/romannumbers/RomannumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.romannumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomannumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
