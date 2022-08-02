55c19c3ec37c3fcfb392eeff9a06742455db7e4e
LucasAbbiaticiMeli <lucas.abbiatici@mercadolibre.com>
2022-08-02 16:01:31 -0300

    spring - ejercicio codigo morse

---
+++ 3. spring/codigo_morse/src/main/java/com/spring/codigo_morse/CodigoMorseController.java
@@ -0,0 +1,58 @@
+package com.spring.codigo_morse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CodigoMorseController {
+
+    @GetMapping("/{morse}")
+    public String codigoMorseALetras(@PathVariable String morse){
+
+        String str = "";
+
+        Map<String, String> morseCodeMap = new HashMap<>();
+        morseCodeMap.put(".-","A");
+        morseCodeMap.put("-...","B");
+        morseCodeMap.put("-.-.","C");
+        morseCodeMap.put("-..","D");
+        morseCodeMap.put(".","E");
+        morseCodeMap.put("..-.","F");
+        morseCodeMap.put("--.","G");
+        morseCodeMap.put("....","H");
+        morseCodeMap.put("..","I");
+        morseCodeMap.put(".---","J");
+        morseCodeMap.put("-.-","K");
+        morseCodeMap.put(".-..","L");
+        morseCodeMap.put("--","M");
+        morseCodeMap.put("-.","N");
+        morseCodeMap.put("---","0");
+        morseCodeMap.put(".--.","P");
+        morseCodeMap.put("--.-","Q");
+        morseCodeMap.put(".-.","R");
+        morseCodeMap.put("...","S");
+        morseCodeMap.put("-","T");
+        morseCodeMap.put("..-","U");
+        morseCodeMap.put("...-","V");
+        morseCodeMap.put(".--","W");
+        morseCodeMap.put("-..-","X");
+        morseCodeMap.put("-.--","Y");
+        morseCodeMap.put("--..","Z");
+
+        String[] words = morse.split("   ");
+        for(int i = 0; i < words.length; i++){
+            String[] letters = words[i].split(" ");
+            for(int j = 0; j < letters.length; j++){
+                str += morseCodeMap.get(letters[j]);
+            }
+            str += " ";
+        }
+
+        return str;
+    }
+
+}
---
+++ 3. spring/codigo_morse/src/test/java/com/spring/codigo_morse/CodigoMorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.codigo_morse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoMorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
