8306c578b93db9f4b623d5db92b907cac53b33c5
Gianni <gianni.tiezzi@mercadolibre.com>
2022-08-02 01:51:54 -0300

    Cambio la manera en la que se muestran las series.

--- 2. java/Bootcamp/src/integrador/series_numericas/Main.java
+++ 2. java/Bootcamp/src/integrador/series_numericas/Main.java
@@ -6,19 +6,20 @@ import integrador.series_numericas.model.Serie3;
 
 public class Main {
     public static void main(String[] args) {
+
         Serie2 serie2 = new Serie2();
         for (int i = 0; i < 4; i++) {
-            System.out.println(serie2.siguiente());
+            System.out.printf("%s" + (i==3 ? "\n" : ", "), serie2.siguiente());
         }
 
         Serie1 serie1 = new Serie1();
         for (int i = 0; i < 4; i++) {
-            System.out.println(serie1.siguiente());
+            System.out.printf("%s" + (i==3 ? "\n" : ", "), serie1.siguiente());
         }
 
         Serie3 serie3 = new Serie3();
         for (int i = 0; i < 4; i++) {
-            System.out.println(serie3.siguiente());
+            System.out.printf("%s" + (i==3 ? "\n" : ", "), serie3.siguiente());
         }
 
     }
