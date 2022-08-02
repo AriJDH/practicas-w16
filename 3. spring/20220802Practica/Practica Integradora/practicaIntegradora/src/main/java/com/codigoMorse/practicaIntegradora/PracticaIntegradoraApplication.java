131465d889dec1439c6d1e9965a9b8e33f76c4b9
Your Name <you@example.com>
2022-08-02 14:09:10 -0500

    add project spring morse

---
+++ 3. spring/20220802Practica/Practica Integradora/practicaIntegradora/src/main/java/com/codigoMorse/practicaIntegradora/PracticaIntegradoraApplication.java
@@ -0,0 +1,13 @@
+package com.codigoMorse.practicaIntegradora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaIntegradoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaIntegradoraApplication.class, args);
+	}
+
+}
