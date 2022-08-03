4d8b557103d99031dde081e297ebd1c181c2c867
Mateo Bica <mateo.bica@mercadolibre.com>
2022-08-03 14:01:56 -0300

    Ejercicio deportistas DTO

---
+++ 3. spring/Deportistas/src/main/java/com/bootcamp/deportistas/entities/Sport.java
@@ -0,0 +1,15 @@
+package com.bootcamp.deportistas.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sport {
+
+    private String name;
+    private String level;
+
+}
