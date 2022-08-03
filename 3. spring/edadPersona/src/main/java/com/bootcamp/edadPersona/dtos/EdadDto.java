2aaddf4b6ab2ec1e9c16ebef0b1f318de475b6bd
KevGamarra <kevin.molano@mercadolibre.com.co>
2022-08-03 10:41:05 -0500

    Edad de una Persona DTO

---
+++ 3. spring/edadPersona/src/main/java/com/bootcamp/edadPersona/dtos/EdadDto.java
@@ -0,0 +1,14 @@
+package com.bootcamp.edadPersona.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EdadDto {
+  private Integer edad;
+}
