13c78f8ac3446be68794e72525b1916f2b6b511a
ndemaioml <nicolas.demaio@mercadolibre.com>
2022-08-03 10:53:18 -0300

    Ejercicio 1: Calcular edad

---
+++ 3. spring/ejercicios-dto-y-response-entity/src/main/java/com/ndemaio/ejerciciosdtoyresponseentity/EjerciciosDtoYResponseEntityApplication.java
@@ -0,0 +1,13 @@
+package com.ndemaio.ejerciciosdtoyresponseentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosDtoYResponseEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjerciciosDtoYResponseEntityApplication.class, args);
+    }
+
+}
