3ecee20e30553e008208c17b5efd1ea882eb793f
ana <ana.delgado@mercadolibre.com.co>
2022-08-03 09:46:03 -0500

    Ejercicio REST Spring edad persona

---
+++ 3. spring/AplicacionEdadPersona/src/main/java/com/bootcamp/AplicacionEdadPersona/AplicacionEdadPersonaApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.AplicacionEdadPersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionEdadPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionEdadPersonaApplication.class, args);
+	}
+
+}
---
+++ 3. spring/AplicacionEdadPersona/src/test/java/com/bootcamp/AplicacionEdadPersona/AplicacionEdadPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.AplicacionEdadPersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionEdadPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
