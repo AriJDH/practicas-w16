606a1e91b154301ce66b05d43ac25c4cd9669fd6
KevGamarra <kevin.molano@mercadolibre.com.co>
2022-08-03 09:13:14 -0500

    Ejercicio edad persona

---
+++ 3. spring/edadPersona/src/test/java/com/bootcamp/edadPersona/EdadPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.edadPersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdadPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
