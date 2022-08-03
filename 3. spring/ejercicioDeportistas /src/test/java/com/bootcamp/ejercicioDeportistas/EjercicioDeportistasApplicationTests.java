a001aa377e4771e15bed0c3caee71f1819792d0c
KevGamarra <kevin.molano@mercadolibre.com.co>
2022-08-03 11:48:56 -0500

    DTO, ResponseEntity y repaso general

---
+++ 3. spring/ejercicioDeportistas /src/test/java/com/bootcamp/ejercicioDeportistas/EjercicioDeportistasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.ejercicioDeportistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioDeportistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
