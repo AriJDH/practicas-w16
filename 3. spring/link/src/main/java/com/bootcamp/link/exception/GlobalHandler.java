ee55b055b69eaadd130b35c1909a965e419614f3
rnegreyra <rodrigo.negreyra@mercadolibre.com>
2022-08-08 16:10:13 -0300

    implementacion del ejercicio del youtuber (Excepciones)

--- 3. spring/link/src/main/java/com/bootcamp/link/exception/GlobalHandler.java
+++ 3. spring/link/src/main/java/com/bootcamp/link/exception/GlobalHandler.java
@@ -5,8 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 
-@ControllerAdvice()
+@ControllerAdvice(annotations = RestController.class)
 public class GlobalHandler {
 
     @ExceptionHandler(InvalidLinkException.class)
