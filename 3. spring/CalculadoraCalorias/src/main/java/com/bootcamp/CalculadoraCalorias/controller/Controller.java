dfd26f5c547d5b3dee30607a097e103dbc09c54e
Claudio Antolin <claudio.antolin@mercadolibre.cl>
2022-08-08 00:45:08 -0400

    exercise 'CalculadoraCalorias' uploaded

--- 3. spring/CalculadoraCalorias/src/main/java/com/bootcamp/CalculadoraCalorias/controller/Controller.java
+++ 3. spring/CalculadoraCalorias/src/main/java/com/bootcamp/CalculadoraCalorias/controller/Controller.java
@@ -4,6 +4,8 @@ import com.bootcamp.CalculadoraCalorias.dto.IngredienteDTO;
 import com.bootcamp.CalculadoraCalorias.dto.PlatoDTO;
 import com.bootcamp.CalculadoraCalorias.service.Servicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,13 +15,16 @@ public class Controller {
     private Servicio service;
 
     @GetMapping("/{plato}")
-    public PlatoDTO mostrarPlato(@PathVariable String plato){
-        return service.getPlatoDTO(plato);
+    public ResponseEntity<PlatoDTO> mostrarPlato(@PathVariable String plato){
+        return new ResponseEntity<>(service.getPlatoDTO(plato), HttpStatus.OK);
+
     }
 
     @RequestMapping("/{valor1}/{valor2}/{valor3}")
-    public List<PlatoDTO> arregloPlatos(@PathVariable String valor1, @PathVariable String valor2, @PathVariable String valor3){
-        return service.getListaPlatosDTO(valor1, valor2, valor3);
+    public ResponseEntity<List<PlatoDTO>> arregloPlatos(@PathVariable String valor1,
+                                                        @PathVariable String valor2,
+                                                        @PathVariable String valor3){
+        return new ResponseEntity<>(service.getListaPlatosDTO(valor1, valor2, valor3), HttpStatus.OK);
     }
 
 }
