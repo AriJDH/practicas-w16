418487d9a01c5dcdfd2bcb8fb4db58c25226d986
ppereyraok <pamela.pereyra@mercadolibre.com>
2022-08-08 15:52:05 -0300

    Se agrega profiles

--- 3. spring/profilesP2PG/src/main/java/com/profiles/profilesP2PG/config/JavaDevConfig.java
+++ 3. spring/profilesP2PG/src/main/java/com/profiles/profilesP2PG/config/JavaDevConfig.java
@@ -3,8 +3,15 @@ package com.profiles.profilesP2PG.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import javax.annotation.PostConstruct;
+
 @Profile("dev")
 @Configuration
 public class JavaDevConfig {
 
+    //configuracion cuando arranque el perfil
+    @PostConstruct
+    public void initialize (){
+        System.out.println("---- Iniciando ambiente DEV -----");
+    }
 }
