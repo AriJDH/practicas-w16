20dbfdd9e463b36fa82888eb6d417ee1f26c73ce
javgomez <javier.gomez@mercadolibre.com>
2022-08-09 13:24:41 -0300

    Added intefaces for repos

---
+++ 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/repository/IPostRepository.java
@@ -0,0 +1,15 @@
+package com.example.be_java_hisp_w16_g09.repository;
+
+import com.example.be_java_hisp_w16_g09.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface IPostRepository {
+
+    void createElement(Post newPost);
+
+    List<Post> searchById(int id);
+
+
+}
