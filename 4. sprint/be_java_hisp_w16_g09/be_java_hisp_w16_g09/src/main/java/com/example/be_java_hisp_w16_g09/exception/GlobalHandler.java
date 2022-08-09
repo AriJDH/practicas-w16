881b04904151849d590a111125d36c88518dc2a6
javgomez <javier.gomez@mercadolibre.com>
2022-08-09 16:04:06 -0300

    Exception for user not found handler

--- 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/exception/GlobalHandler.java
+++ 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/exception/GlobalHandler.java
@@ -1,7 +1,17 @@
 package com.example.be_java_hisp_w16_g09.exception;
 
+import com.example.be_java_hisp_w16_g09.dto.MessageDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<MessageDto> catchUserNotFound(UserNotFoundException exception) {
+        MessageDto response = new MessageDto(exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
