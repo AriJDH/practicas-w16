1cb853c2990f4e7d828db4932101e2b55ea6e9b7
Santiago Torres <santiago.torresgiraldo@mercadolibre.com.co>
2022-08-09 16:32:51 -0500

    Added methods for user follow and unfollow

--- 4. sprint/be_java_hisp_w16_g7/src/main/java/com/bootcamp/be_java_hisp_w16_g7/repository/UserRepository.java
+++ 4. sprint/be_java_hisp_w16_g7/src/main/java/com/bootcamp/be_java_hisp_w16_g7/repository/UserRepository.java
@@ -14,9 +14,39 @@ public class UserRepository implements IUserRepository{
     public UserRepository() {
         user = new ArrayList<>();
 
-        user.add(new User(1111, "User1", List.of(), List.of(), List.of()));
-        user.add(new User(2222, "User2", List.of(), List.of(), List.of()));
-        user.add(new User(3333, "User3", List.of(), List.of(), List.of()));
-        user.add(new User(4444, "User4", List.of(), List.of(), List.of()));
+        user.add(new User(1111, "User1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+        user.add(new User(2222, "User2", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+        user.add(new User(3333, "User3", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+        user.add(new User(4444, "User4", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
     }
+
+    @Override
+    public User findUserById(int userId) {
+        User user = this.user.stream().filter(u -> u.getId()==userId).findFirst().orElse(null);
+        return user;
+    }
+
+    @Override
+    public void addToUserFollowers(User userToAdd, User user) {
+        user.getFollowers().add(userToAdd);
+    }
+
+    @Override
+    public void addToUserFollows(User userToAdd, User user) {
+        user.getFollows().add(userToAdd);
+    }
+
+    @Override
+    public void removeFromUserFollowers(User userToRemove, User user) {
+        user.getFollowers().remove(userToRemove);
+
+    }
+
+    @Override
+    public void removeFromUserFollows(User userToRemove, User user) {
+        user.getFollows().remove(userToRemove);
+
+    }
+
+
 }
