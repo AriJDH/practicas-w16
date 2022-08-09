bffbf7699ff467d0acf6e4a6ae04aa64e3d6563f
javgomez <javier.gomez@mercadolibre.com>
2022-08-09 13:18:51 -0300

    implemented exception handling

---
+++ 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/exception/GlobalHandler.java
@@ -0,0 +1,7 @@
+package com.example.be_java_hisp_w16_g09.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalHandler {
+}
---
+++ 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/exception/UserNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.be_java_hisp_w16_g09.exception;
+
+public class UserNotFoundException extends RuntimeException{
+}
