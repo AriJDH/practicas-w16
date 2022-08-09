ba076536802961391e35d10a96e08ce6454ca752
javgomez <javier.gomez@mercadolibre.com>
2022-08-09 13:36:37 -0300

    implemented services layer

---
+++ 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/service/PostService.java
@@ -0,0 +1,32 @@
+package com.example.be_java_hisp_w16_g09.service;
+
+import com.example.be_java_hisp_w16_g09.repository.IPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    @Autowired
+    IPostRepository postRepository;
+
+
+    //Javi
+
+
+    //Martin
+
+
+    //MaxiM
+
+
+    //MaxiN
+
+
+    //Guille
+
+
+    //Nico
+
+
+
+}
---
+++ 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.be_java_hisp_w16_g09.service;
+
+import com.example.be_java_hisp_w16_g09.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    IUserRepository userRepository;
+
+    //Javi
+
+
+    //Martin
+
+
+    //MaxiM
+
+
+    //MaxiN
+
+
+    //Guille
+
+
+    //Nico
+
+
+
+}
