f1c941cc0709b5c5f6b76a155ffb18bfd63433fd
emarivero1 <emanuel.rivero@mercadolibre.com>
2022-08-09 15:52:19 -0300

    add UserNotExistException

--- 4. sprint/be_java_hisp_w16_g03/src/main/java/com/example/be_java_hisp_w16_g03/service/UserService.java
+++ 4. sprint/be_java_hisp_w16_g03/src/main/java/com/example/be_java_hisp_w16_g03/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.be_java_hisp_w16_g03.service;
 import com.example.be_java_hisp_w16_g03.dto.FollowersDTO;
 import com.example.be_java_hisp_w16_g03.dto.UserDTO;
 import com.example.be_java_hisp_w16_g03.entity.User;
+import com.example.be_java_hisp_w16_g03.exception.UserNotExistException;
 import com.example.be_java_hisp_w16_g03.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class UserService implements IUserService{
     public FollowersDTO getFollowers(Integer id) {
         User user=repository.getUserById(id);
         if (user==null){
-            return null;
+            throw new UserNotExistException(id);
         }
         List<UserDTO> userDTO= user.getFollowers().stream().map(userA->{
             UserDTO userDTO1=new UserDTO(userA.getUserId(),userA.getUserName());
