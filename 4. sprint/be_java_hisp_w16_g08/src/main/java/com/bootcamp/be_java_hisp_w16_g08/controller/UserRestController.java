e5bf374b85b9a4a9837a51c533a1756a8cf9a6e8
fdiaz1996 <facundodaniel.diaz@mercadolibre.com>
2022-08-09 18:20:50 -0300

    Add search vendors follower

--- 4. sprint/be_java_hisp_w16_g08/src/main/java/com/bootcamp/be_java_hisp_w16_g08/controller/UserRestController.java
+++ 4. sprint/be_java_hisp_w16_g08/src/main/java/com/bootcamp/be_java_hisp_w16_g08/controller/UserRestController.java
@@ -1,6 +1,7 @@
 package com.bootcamp.be_java_hisp_w16_g08.controller;
 
 import com.bootcamp.be_java_hisp_w16_g08.dto.ResponseVendorsFollowedDto;
+import com.bootcamp.be_java_hisp_w16_g08.dto.response.UserFollowers;
 import com.bootcamp.be_java_hisp_w16_g08.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,11 @@ public class UserRestController {
     @GetMapping("/users/{userId}/followed/list")
     public ResponseEntity<ResponseVendorsFollowedDto> getVendorsFollowedByUser (@PathVariable int userId){
         return new ResponseEntity<>(userService.getVendorsFollowedByUser(userId), HttpStatus.OK);
+    }
+    @GetMapping("/users/{userId}/followers/list")
+    public ResponseEntity<UserFollowers> requestAllFollowers(@PathVariable int userId){
+
+        return new ResponseEntity<>(userService.getAllFollowers(userId), HttpStatus.OK);
     }
 
 }
--- 4. sprint/be_java_hisp_w16_g08/src/main/java/com/bootcamp/be_java_hisp_w16_g08/service/UserService.java
+++ 4. sprint/be_java_hisp_w16_g08/src/main/java/com/bootcamp/be_java_hisp_w16_g08/service/UserService.java
@@ -2,9 +2,12 @@ package com.bootcamp.be_java_hisp_w16_g08.service;
 
 import com.bootcamp.be_java_hisp_w16_g08.dto.ResponseUserInformationDto;
 import com.bootcamp.be_java_hisp_w16_g08.dto.ResponseVendorsFollowedDto;
+import com.bootcamp.be_java_hisp_w16_g08.dto.response.UserFollowers;
+import com.bootcamp.be_java_hisp_w16_g08.dto.response.UserFollowersList;
 import com.bootcamp.be_java_hisp_w16_g08.entiry.User;
 import com.bootcamp.be_java_hisp_w16_g08.exception.UserNotFoundException;
 import com.bootcamp.be_java_hisp_w16_g08.repository.IUserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +19,7 @@ public class UserService implements IUserService{
 
     @Autowired
     IUserRepository userRepository;
-
+    ModelMapper mapper= new ModelMapper();
     @Override
     public ResponseVendorsFollowedDto getVendorsFollowedByUser(int userId) {
 
@@ -34,5 +37,17 @@ public class UserService implements IUserService{
 
     public boolean isVendor(User user){
         return user.getPostMade().size()>0;
+    }
+
+
+    @Override
+    public UserFollowers getAllFollowers(int id) {
+        if (!userRepository.isPresent(id))
+            throw new UserNotFoundException();
+
+        return new UserFollowers(id, userRepository.getUserById(id).getName(), userRepository.getUserById(id).getFollowerList().stream()
+                .filter(x->isVendor(userRepository.getUserById(id)))
+                .map(user -> mapper.map(user,UserFollowersList.class))
+                .collect(Collectors.toList()));
     }
 }
