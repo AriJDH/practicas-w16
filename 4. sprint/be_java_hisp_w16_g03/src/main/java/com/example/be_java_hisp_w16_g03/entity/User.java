7d12209ebee0969d78b7f241e7257adc28d84fc2
Joaquin Antonio Caetano <jocaetano@uy0fvfhk2htq05p.ml.com>
2022-08-09 16:57:36 -0300

    add validate to user

--- 4. sprint/be_java_hisp_w16_g03/src/main/java/com/example/be_java_hisp_w16_g03/entity/User.java
+++ 4. sprint/be_java_hisp_w16_g03/src/main/java/com/example/be_java_hisp_w16_g03/entity/User.java
@@ -2,12 +2,12 @@ package com.example.be_java_hisp_w16_g03.entity;
 
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class User {
     private Integer userId;
@@ -15,6 +15,28 @@ public class User {
     private List<User> followers;
     private List<User> followeds;
     private List<Post> posts;
+
+
+    public List<User> validateFollowers() {
+        if (this.followers == null) {
+            this.followers = new ArrayList<>();
+        }
+        return this.followers;
+    }
+
+    public List<User> validateFolloweds() {
+        if (this.followeds == null) {
+            this.followeds = new ArrayList<>();
+        }
+        return this.followeds;
+    }
+
+    public List<Post> validatePosts() {
+        if (this.posts == null) {
+            this.posts = new ArrayList<>();
+        }
+        return this.posts;
+    }
 
 
 }
