c2f1b2c97dafb0dd1f49382148b1dfc814081430
mnicoletta <maximiliano.nicoletta@mercadolibre.com>
2022-08-09 14:36:05 -0300

    add function for User Created

--- 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/repository/UserRepository.java
+++ 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/repository/UserRepository.java
@@ -3,23 +3,38 @@ package com.example.be_java_hisp_w16_g09.repository;
 import com.example.be_java_hisp_w16_g09.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Repository
-public class UserRepository implements IUserRepository{
+public class UserRepository implements IUserRepository {
     private HashMap<Integer, User> users = new HashMap<>();
 
-    public User createElement(User newUser){
-        newUser.setUserId(users.size()+1);
+    public User createElement(User newUser) {
+        newUser.setUserId(users.size() + 1);
         users.put(newUser.getUserId(), newUser);
         return newUser;
     }
 
-    public User searchById(int id){
+    public User searchById(int id) {
         return users.get(id);
     }
 
-    public void updateUser(User user){
-        users.put(user.getUserId(),user);
+    public void updateUser(User user) {
+        users.put(user.getUserId(), user);
+    }
+    public HashMap<Integer, User> addUsers() {
+        User userA = new User(1,"Usuario1",new ArrayList<>(),new ArrayList<>());
+        User userB = new User(2,"Usuario2",new ArrayList<>(),new ArrayList<>());
+        User userC = new User(3,"Usuario3",new ArrayList<>(),new ArrayList<>());
+        User userD = new User(4,"Usuario4",new ArrayList<>(),new ArrayList<>());
+        User userE = new User(5,"Usuario5",new ArrayList<>(),new ArrayList<>());
+        users.put(1,userA);
+        users.put(2,userB);
+        users.put(3,userC);
+        users.put(4,userD);
+        users.put(5,userE);
+        return users;
     }
 }
