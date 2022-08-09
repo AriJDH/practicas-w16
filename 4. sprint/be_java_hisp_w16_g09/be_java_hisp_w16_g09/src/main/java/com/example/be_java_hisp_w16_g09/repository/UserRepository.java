3b8f4cb985dad2fc8839bb02e90ffc6acadb9797
Guillermo Bariani <guillermo.bariani@mercadolibre.com>
2022-08-09 16:20:40 -0300

    added users in construct

--- 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/repository/UserRepository.java
+++ 4. sprint/be_java_hisp_w16_g09/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/repository/UserRepository.java
@@ -4,12 +4,17 @@ import com.example.be_java_hisp_w16_g09.model.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 @Repository
 public class UserRepository implements IUserRepository {
     private HashMap<Integer, User> users = new HashMap<>();
+
+    public UserRepository() {
+        this.addUsers();
+    }
 
     public User createElement(User newUser) {
         newUser.setUserId(users.size() + 1);
@@ -24,17 +29,20 @@ public class UserRepository implements IUserRepository {
     public void updateUser(User user) {
         users.put(user.getUserId(), user);
     }
-    public HashMap<Integer, User> addUsers() {
+    private void addUsers() {
         User userA = new User(1,"Usuario1",new ArrayList<>(),new ArrayList<>());
         User userB = new User(2,"Usuario2",new ArrayList<>(),new ArrayList<>());
         User userC = new User(3,"Usuario3",new ArrayList<>(),new ArrayList<>());
         User userD = new User(4,"Usuario4",new ArrayList<>(),new ArrayList<>());
         User userE = new User(5,"Usuario5",new ArrayList<>(),new ArrayList<>());
+        List<User> followersUserA = new ArrayList<>(Arrays.asList(userB, userC));
+        userA.setFollowers(followersUserA);
+        List<User> followersUserD = new ArrayList<>(Arrays.asList(userB, userE));
+        userD.setFollowers(followersUserD);
         users.put(1,userA);
         users.put(2,userB);
         users.put(3,userC);
         users.put(4,userD);
         users.put(5,userE);
-        return users;
     }
 }
