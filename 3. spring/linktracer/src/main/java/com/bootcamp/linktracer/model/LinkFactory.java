7af4be7d6acf67ba6cb79a0e3084fe925fc4c99b
ser-cardenas <sergio.cardenas@mercadolibre.com.co>
2022-08-09 11:17:20 -0500

    more progress in link exercise

--- 3. spring/linktracer/src/main/java/com/bootcamp/linktracer/model/LinkFactory.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.linktracer.model;
-
-public class LinkFactory {
-    private int counter = 0;
-
-    public Link make(String url, String password) {
-        return new Link(++counter, url, password, 0, true);
-    }
-}
