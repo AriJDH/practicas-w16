29b8ac1653d06cc7fb5a22112ed3342e16b31d85
Your Name <you@example.com>
2022-08-10 16:35:47 -0500

    sprint 1

---
+++ 4. sprint/be_java_hisp_w16_g04/src/main/java/com/bootcamp/be_java_hisp_w16_g04/model/User.java
@@ -0,0 +1,17 @@
+package com.bootcamp.be_java_hisp_w16_g04.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class User {
+  private Integer userId;
+  private String userName;
+}
---
+++ 4. sprint/be_java_hisp_w16_g04/src/main/java/com/bootcamp/be_java_hisp_w16_g04/service/IProductService.java
@@ -0,0 +1,12 @@
+package com.bootcamp.be_java_hisp_w16_g04.service;
+
+import com.bootcamp.be_java_hisp_w16_g04.dto.ProductDTO;
+
+import com.bootcamp.be_java_hisp_w16_g04.dto.RequestCreatePublicationDTO;
+import com.bootcamp.be_java_hisp_w16_g04.dto.ResponseApiDTO;
+
+public interface IProductService {
+  ProductDTO getProductById(Integer idProduct);
+
+  ResponseApiDTO CreateProduct(RequestCreatePublicationDTO publicationDTO);
+}
