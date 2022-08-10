0eb7d5baab2075290692e9fd69acfd4ab5201402
danielcondori <daniel.condori@mercadolibre.com>
2022-08-10 16:50:12 -0300

    Agrego trabajo Sprint 1

---
+++ 4. sprint/be_java_hisp_w16_CONDORI/src/main/java/com/bootcamp/be_java_hisp_w16_g08/controller/ProductRestController.java
@@ -0,0 +1,26 @@
+package com.bootcamp.be_java_hisp_w16_g08.controller;
+
+import com.bootcamp.be_java_hisp_w16_g08.dto.response.PostDto;
+import com.bootcamp.be_java_hisp_w16_g08.service.IProductService;
+import com.bootcamp.be_java_hisp_w16_g08.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductRestController {
+
+    final
+    IProductService productService;
+
+    public ProductRestController(IProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/products/post")
+    public void publishPost(@RequestBody PostDto post){
+        productService.publishPost(post);
+    }
+
+}
---
+++ 4. sprint/be_java_hisp_w16_CONDORI/src/main/java/com/bootcamp/be_java_hisp_w16_g08/service/IProductService.java
@@ -0,0 +1,8 @@
+package com.bootcamp.be_java_hisp_w16_g08.service;
+
+import com.bootcamp.be_java_hisp_w16_g08.dto.response.PostDto;
+
+public interface IProductService {
+
+    void publishPost(PostDto post);
+}
