619cf6ff0854079a91ea78c738ca6bac7a4ecf48
achanes <agustin.chanes@mercadolibre.com>
2022-08-10 16:26:35 -0300

    Sprint 1, parte individual first commit

---
+++ 4. sprint/be_java_hisp_w16_g03_chanes/src/main/java/com/example/be_java_hisp_w16_g03/dto/ProductDTO.java
@@ -0,0 +1,31 @@
+package com.example.be_java_hisp_w16_g03.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ProductDTO {
+    private Integer productId;
+    private String productName;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+
+
+    public boolean validate() {
+        return this.productId != null && this.type != null
+                && this.notes != null && this.color != null
+                && this.brand != null && this.productName != null
+                && !this.type.isEmpty() && !this.notes.isEmpty()
+                && !this.color.isEmpty() && !this.brand.isEmpty()
+                && !this.productName.isEmpty();
+    }
+
+}
---
+++ 4. sprint/be_java_hisp_w16_g03_chanes/src/main/java/com/example/be_java_hisp_w16_g03/entity/Product.java
@@ -0,0 +1,18 @@
+package com.example.be_java_hisp_w16_g03.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    private Integer productId;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+    private String productName;
+
+}
