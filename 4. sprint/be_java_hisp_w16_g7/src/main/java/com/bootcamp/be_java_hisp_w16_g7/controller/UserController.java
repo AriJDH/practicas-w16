89a669a7131059f12a5fb6760a9805c7d576f957
ser-cardenas <sergio.cardenas@mercadolibre.com.co>
2022-08-10 11:32:09 -0500

    US 0004 documented

--- 4. sprint/be_java_hisp_w16_g7/src/main/java/com/bootcamp/be_java_hisp_w16_g7/controller/UserController.java
+++ 4. sprint/be_java_hisp_w16_g7/src/main/java/com/bootcamp/be_java_hisp_w16_g7/controller/UserController.java
@@ -5,6 +5,11 @@ import com.bootcamp.be_java_hisp_w16_g7.dto.FollowersCountDto;
 import com.bootcamp.be_java_hisp_w16_g7.dto.FollowersSellersDTO;
 import com.bootcamp.be_java_hisp_w16_g7.dto.ResponseUserFollowedDTO;
 import com.bootcamp.be_java_hisp_w16_g7.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,9 +35,15 @@ public class UserController {
         return new ResponseEntity(userService.unfollow(userId, userIdToUnfollow));
     }
 
+    @Operation(summary = "Get list of followed by given user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List given"),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content),
+    })
     @GetMapping("/{userId}/followed/list")
-    public ResponseEntity<ResponseUserFollowedDTO> getUserFollowedList(@PathVariable int userId,
-                                                                       @RequestParam(required = false) String order) {
+    public ResponseEntity<ResponseUserFollowedDTO> getUserFollowedList(
+            @Parameter(description = "User id") @PathVariable int userId,
+            @Parameter(description = "Order query (name_asc, name_desc)") @RequestParam(required = false) String order) {
         return new ResponseEntity<>(userService.getUserFollowedList(userId, order), HttpStatus.OK);
 
     }
