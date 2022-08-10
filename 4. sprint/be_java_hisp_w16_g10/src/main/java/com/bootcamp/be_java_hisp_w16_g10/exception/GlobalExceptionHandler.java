56a28457931a89f9a716914301a3274039dd28c0
Matias Lamela <matias.lamela@mercadolibre.com>
2022-08-10 12:00:53 -0300

    adding errorHandlerDTO and implementing JSON error message

--- 4. sprint/be_java_hisp_w16_g10/src/main/java/com/bootcamp/be_java_hisp_w16_g10/exception/GlobalExceptionHandler.java
+++ 4. sprint/be_java_hisp_w16_g10/src/main/java/com/bootcamp/be_java_hisp_w16_g10/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.bootcamp.be_java_hisp_w16_g10.exception;
 
+import com.bootcamp.be_java_hisp_w16_g10.dto.response.ErrorHandlerDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -11,32 +12,25 @@ import org.springframework.web.client.HttpClientErrorException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<?> NotFoundException(Exception e){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ErrorHandlerDTO> NotFoundException(Exception e){
+        ErrorHandlerDTO errorHandlerDTO = new ErrorHandlerDTO().builder().message(e.getMessage()).build();
+        return new ResponseEntity<>(errorHandlerDTO, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(FoundException.class)
-    public ResponseEntity<?> FoundException(Exception e){
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    public ResponseEntity<ErrorHandlerDTO> FoundException(Exception e){
+        ErrorHandlerDTO errorHandlerDTO = new ErrorHandlerDTO().builder().message(e.getMessage()).build();
+        return new ResponseEntity<>(errorHandlerDTO, HttpStatus.CONFLICT);
     }
 
-    @ExceptionHandler(UrlNotValidException.class)
-    public ResponseEntity<?> UrlNotValidException(Exception e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<?> ConstraintViolationException(Exception e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
-
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<?> MissingServletRequestParameterException(Exception e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
-
-@ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<?> BadRequestException(Exception e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    @ExceptionHandler({
+            UrlNotValidException.class,
+            ConstraintViolationException.class,
+            MissingServletRequestParameterException.class,
+            BadRequestException.class
+        })
+    public ResponseEntity<ErrorHandlerDTO> UrlNotValidException(Exception e){
+        ErrorHandlerDTO errorHandlerDTO = new ErrorHandlerDTO().builder().message(e.getMessage()).build();
+        return new ResponseEntity<>(errorHandlerDTO, HttpStatus.BAD_REQUEST);
     }
 }
