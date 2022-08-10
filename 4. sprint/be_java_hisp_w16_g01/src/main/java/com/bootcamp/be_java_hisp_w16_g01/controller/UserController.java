0f9c2959688958b9e490994c1cfe3c87a704fca1
Hernan Gonzalez Correa <hernan.gonzalez@mercadolibre.com>
2022-08-10 15:22:38 -0300

    Commit inicial con base de trabajo grupal para iniciar trabajo individual

---
+++ 4. sprint/be_java_hisp_w16_g01/src/main/java/com/bootcamp/be_java_hisp_w16_g01/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.bootcamp.be_java_hisp_w16_g01.controller;
+
+import com.bootcamp.be_java_hisp_w16_g01.dto.*;
+import com.bootcamp.be_java_hisp_w16_g01.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    protected IUserService userService;
+
+    @PostMapping("/{userId}/follow/{userIdToFollow}")
+    public ResponseEntity<MessageDto> addFollower(@PathVariable int userId, @PathVariable int userIdToFollow){
+        return new ResponseEntity<>(userService.addFollower(userId, userIdToFollow),HttpStatus.OK);
+    }
+
+    @PostMapping("/{userId}/unfollow/{userIdToUnfollow}")
+    public ResponseEntity<UserUnfollowDTO> unfollowUser(@PathVariable int userId, @PathVariable int userIdToUnfollow) {
+        return ResponseEntity.ok(userService.unfollowUser(userId, userIdToUnfollow));
+    }
+
+    @GetMapping("/{userId}/followers/list")
+    public ResponseEntity<UserFollowerDTO> getFollowers(@PathVariable int userId, @RequestParam String order) {
+        return new ResponseEntity<>(userService.getFollowers(userId, order), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}/followed/list")
+    public ResponseEntity<UserFollowedDTO> getFollowed(@PathVariable int userId, @RequestParam String order){
+        return new ResponseEntity<>(userService.getFollowed(userId, order), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}/followers/count")
+    public ResponseEntity<FollowersCountDTO> getFollowersCount(@PathVariable int userId){
+        return new ResponseEntity<>(userService.getCantFollowers(userId), HttpStatus.OK);
+    }
+}
---
+++ 4. sprint/be_java_hisp_w16_g01/src/main/java/com/bootcamp/be_java_hisp_w16_g01/repository/IPostRepository.java
@@ -0,0 +1,11 @@
+package com.bootcamp.be_java_hisp_w16_g01.repository;
+
+import com.bootcamp.be_java_hisp_w16_g01.entities.Post;
+
+import java.util.List;
+
+public interface IPostRepository {
+     int createPost(Post post);
+     List<Post> getPostsByUserId(int userId);
+
+}
