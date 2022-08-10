2beedecc83291eb2186b3f8d6f749c5f4437f5d6
fdiaz1996 <facundodaniel.diaz@mercadolibre.com>
2022-08-10 16:55:12 -0300

    Agregando proyecto

---
+++ 4. sprint/be_java_hisp_w16_g08/src/main/java/com/bootcamp/be_java_hisp_w16_g08/dto/response/ResponseUserInformationDto.java
@@ -0,0 +1,18 @@
+package com.bootcamp.be_java_hisp_w16_g08.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseUserInformationDto {
+        private int userId;
+        private String userName;
+
+}
---
+++ 4. sprint/be_java_hisp_w16_g08/src/main/java/com/bootcamp/be_java_hisp_w16_g08/service/ProductService.java
@@ -0,0 +1,61 @@
+package com.bootcamp.be_java_hisp_w16_g08.service;
+
+import com.bootcamp.be_java_hisp_w16_g08.dto.response.PostDto;
+import com.bootcamp.be_java_hisp_w16_g08.dto.response.ProductDto;
+import com.bootcamp.be_java_hisp_w16_g08.entiry.Post;
+import com.bootcamp.be_java_hisp_w16_g08.entiry.Product;
+import com.bootcamp.be_java_hisp_w16_g08.entiry.User;
+import com.bootcamp.be_java_hisp_w16_g08.exception.UserNotFoundException;
+import com.bootcamp.be_java_hisp_w16_g08.repository.IPostRepository;
+import com.bootcamp.be_java_hisp_w16_g08.repository.IUserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class ProductService implements IProductService{
+
+    final
+    IPostRepository postRepository;
+    final
+    IUserRepository userRepository;
+
+    private int postIdCount = 0;
+
+    public ProductService(IPostRepository postRepository, IUserRepository userRepository){
+        User user1 = new User(1,"User1",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+        User user2 = new User(2,"User2",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+        User user3 = new User(3,"User3",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+        User user4 = new User(4,"User4",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+        User user5 = new User(5,"User5",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+
+        userRepository.addUser(user1);
+        userRepository.addUser(user2);
+        userRepository.addUser(user3);
+        userRepository.addUser(user4);
+        userRepository.addUser(user5);
+
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+    @Override
+    public void publishPost(PostDto post){
+        User postUser = getUserIfExist(post.getUserId());
+        postIdCount++;
+        ProductDto pDto = post.getProduct();
+        Product asociatedProduct = new Product(pDto.getProductId(),pDto.getProductName(),
+                                                pDto.getType(),pDto.getBrand(),pDto.getColor(),pDto.getNotes());
+
+        Post newPost = new Post(postIdCount,postUser,post.getDate(),post.getCategory(),post.getPrice(),asociatedProduct);
+        postUser.getPostMade().add(newPost);
+        postRepository.addPost(newPost);
+    }
+
+    private User getUserIfExist(int idUser) {
+        if (!userRepository.isPresent(idUser)) {
+            throw new UserNotFoundException();
+        }
+        return userRepository.getUserById(idUser);
+    }
+
+}
