f8fb339cac00975722945c495a2c0c5d4c8b9213
anmuse <andres.amuse@mercadolibre.com>
2022-08-10 15:12:56 -0300

    Sprint I, trabajo en grupo

---
+++ 4. sprint/be_java_hisp_w16_g03/src/main/java/com/example/be_java_hisp_w16_g03/repository/IUserRepository.java
@@ -0,0 +1,18 @@
+package com.example.be_java_hisp_w16_g03.repository;
+
+import com.example.be_java_hisp_w16_g03.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserRepository {
+    List<User> postData(List<User> users);
+
+    List<User> initData();
+
+    List<User> getAllUser();
+
+    Optional<User> getUserById(Integer id);
+
+    List<User> getFollowedsByUserId(Integer userId);
+}
