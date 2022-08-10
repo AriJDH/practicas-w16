1aede182addf4882419fe5340755c545df7944e3
mlucena7 <martin.lucena@mercadolibre.com>
2022-08-10 16:31:02 -0300

    add excersices Sprint 1

---
+++ 4. sprint/be_java_hisp_w16_g09/src/main/java/com/example/be_java_hisp_w16_g09/dto/MessageDto.java
@@ -0,0 +1,15 @@
+package com.example.be_java_hisp_w16_g09.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class MessageDto {
+    String message;
+}
