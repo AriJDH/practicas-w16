e1c45ef3a420500949fb1eb8ea89ecf792e9bc5e
Tomas Octavio Rodriguez Herrera <tomas.rodriguez@mercadolibre.com.co>
2022-08-11 09:57:35 -0500

    Proyecto grupal

---
+++ 4. sprint/be_java_hisp_w16_g06/src/main/java/com/bootcamp/be_java_hisp_w16_g06/service/ISocialMeliServiceE2.java
@@ -0,0 +1,16 @@
+package com.bootcamp.be_java_hisp_w16_g06.service;
+
+import com.bootcamp.be_java_hisp_w16_g06.dto.FollowedDTO;
+import com.bootcamp.be_java_hisp_w16_g06.dto.FollowersCountDTO;
+import com.bootcamp.be_java_hisp_w16_g06.dto.UserDTO;
+
+import java.util.List;
+
+public interface ISocialMeliServiceE2 {
+
+    List<UserDTO> findById(int userId);
+
+    FollowersCountDTO userFollowers(Integer userId);
+
+    public FollowedDTO userFollowed(int userId);
+}
