04dcaf1b77dfb62a5b8e4da7a8a5bae6c09e9868
Aldo Castagnoli <aldo.castagnoli@mercadolibre.cl>
2022-08-11 10:32:55 -0400

    Trabajo grupal

---
+++ 4. sprint/sprint1g06/src/main/java/com/bootcamp/be_java_hisp_w16_g06/exceptions/FollowedNotFounException.java
@@ -0,0 +1,8 @@
+package com.bootcamp.be_java_hisp_w16_g06.exceptions;
+
+public class FollowedNotFounException extends RuntimeException {
+
+    public FollowedNotFounException(String message) {
+        super(message);
+    }
+}
---
+++ 4. sprint/sprint1g06/src/main/java/com/bootcamp/be_java_hisp_w16_g06/service/ISocialMeliServiceE1.java
@@ -0,0 +1,13 @@
+package com.bootcamp.be_java_hisp_w16_g06.service;
+
+import com.bootcamp.be_java_hisp_w16_g06.dto.FollowIdDto;
+import com.bootcamp.be_java_hisp_w16_g06.dto.ResponseDTO;
+
+public interface ISocialMeliServiceE1 {
+    ResponseDTO followUser(FollowIdDto followIdDto);
+
+    ResponseDTO unFollowUser(FollowIdDto followIdDto);
+
+    boolean findById(int userId);
+
+}
