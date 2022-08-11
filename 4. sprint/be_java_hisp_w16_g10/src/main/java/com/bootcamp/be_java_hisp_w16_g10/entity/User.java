7229e733adde58c6f5052e6672a23ad6aec64f48
Matias Lamela <matias.lamela@mercadolibre.com>
2022-08-11 15:59:01 -0300

    adding the grupal project

---
+++ 4. sprint/be_java_hisp_w16_g10/src/main/java/com/bootcamp/be_java_hisp_w16_g10/entity/User.java
@@ -0,0 +1,23 @@
+package com.bootcamp.be_java_hisp_w16_g10.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String userName;
+    private List<User> followers;
+    private List<User> followed;
+
+    public Integer countFollowers(){
+        return this.followers.size();
+    }
+}
