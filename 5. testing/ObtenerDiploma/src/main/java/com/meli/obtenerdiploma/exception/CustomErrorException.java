20b3aa30c51a2690b020e0572e6cb6b5274392b4
Guillermo Bariani <guillermo.bariani@mercadolibre.com>
2022-08-12 12:15:42 -0300

    add valid exceptions for test exercise

---
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/exception/CustomErrorException.java
@@ -0,0 +1,17 @@
+package com.meli.obtenerdiploma.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter@Setter
+public class CustomErrorException {
+    public CustomErrorException(String message, List<String> details) {
+        super();
+        this.message = message;
+        this.details = details;
+    }
+    private String message;
+    private List<String> details;
+}
---
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/exception/GlobalHandlerException.java
@@ -0,0 +1,28 @@
+package com.meli.obtenerdiploma.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class GlobalHandlerException extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        for(ObjectError error : ex.getBindingResult().getAllErrors()) {
+            details.add(error.getDefaultMessage());
+        }
+        CustomErrorException error = new CustomErrorException("No se han ingresados datos validos.", details);
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+
+}
