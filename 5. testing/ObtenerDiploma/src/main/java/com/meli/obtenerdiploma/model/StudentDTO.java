92725705235312f59fa8db543000476938dcd733
LucasAbbiaticiMeli <lucas.abbiatici@mercadolibre.com>
2022-08-12 13:07:34 -0300

    testing - validaciones, ejercicio obtener diploma upate 3 regexp ahora acepta tildes

--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class StudentDTO {
 
     @NotEmpty(message = "El nombre del alumno no puede estar vacío.")
-    @Pattern(regexp = "^[A-Z]+[a-zA-Z]*$", message = "El nombre del alumno debe comenzar con mayúscula.")
+    @Pattern(regexp = "([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "El nombre del alumno debe comenzar con mayúscula.")
     @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
     private String studentName;
 
--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.*;
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class SubjectDTO {
 
-    @Pattern(regexp = "^[A-Z]+[a-zA-Z]*$", message = "El nombre de la materia debe comenzar con mayúscula.")
+    @Pattern(regexp = "([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "El nombre de la materia debe comenzar con mayúscula.")
     @NotEmpty(message = "El nombre de la materia no puede estar vacío.")
     @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.")
     private String name;
