0c45713cdcda159b9ab8b485bf34f95789a44cd2
KarenGlezM <karen.agonzalez@mercadolibre.com.mx>
2022-08-15 20:34:00 -0500

    Ejercicio Starwars

---
+++ 3. spring/starwars/src/main/java/com/bootcamp/starwars/dtos/PersonajeDTO.java
@@ -0,0 +1,18 @@
+package com.bootcamp.starwars.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonajeDTO {
+
+    private String name;
+    private Integer height;
+    private Integer mass;
+    private String gender;
+    private String homeWorld;
+    private String species;
+}
---
+++ 3. spring/starwars/src/main/java/com/bootcamp/starwars/repositories/PersonajeRepo.java
@@ -0,0 +1,37 @@
+package com.bootcamp.starwars.repositories;
+
+import com.bootcamp.starwars.models.Personaje;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+@Repository
+public class PersonajeRepo {
+
+    public PersonajeRepo (){
+    }
+    public List<Personaje> loadCharacters(){
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:starwars.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<Personaje>> typeRef = new TypeReference<>() {};
+        List<Personaje> characters = null;
+        try {
+            characters = objectMapper.readValue(file, typeRef);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return characters;
+    }
+
+}
