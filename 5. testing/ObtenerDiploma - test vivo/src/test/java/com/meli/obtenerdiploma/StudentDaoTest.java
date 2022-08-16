075c979109778bab726414989701237a517a0b40
javgomez <javier.gomez@mercadolibre.com>
2022-08-16 16:27:42 -0300

    validations

---
+++ 5. testing/ObtenerDiploma - test vivo/src/test/java/com/meli/obtenerdiploma/StudentDaoTest.java
@@ -0,0 +1,17 @@
+package com.meli.obtenerdiploma;
+
+import com.meli.obtenerdiploma.repository.StudentDAO;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class StudentDaoTest {
+
+    private StudentDAO studentDAO;
+
+    @Test
+    public void test() {
+        
+    }
+}
---
+++ 5. testing/ObtenerDiploma pg1/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -0,0 +1,22 @@
+package com.meli.obtenerdiploma.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.List;
+
+@Getter
+@Setter
+public class StudentDTO {
+    @NotEmpty(message = "El nombre del alumno no puede estar vacío")
+    @Pattern(regexp="([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "El nombre del estudiante debe comenzar con mayúscula.")
+    @Size(max=50,message = "La longitud del nombre no puede superar los 50 caracteres.")
+    String studentName;
+    String message;
+    Double averageScore;
+    @NotEmpty(message = "La lista no puede ser vacía.")
+    List<@Valid SubjectDTO> subjects;
+}
