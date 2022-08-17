1d9d908512c7662137aaa8162b9b4bf837db5b31
ana <ana.delgado@mercadolibre.com.co>
2022-08-16 19:39:17 -0500

    Actualizaciones en ejercicios POO, Spring y Testing

---
+++ 5. testing/EJERCICIO_VALIDACIONES/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -0,0 +1,27 @@
+package com.meli.obtenerdiploma.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class StudentDTO {
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
+    @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres")
+    @Pattern(regexp = "([A-Z|Ñ|Á|Ó|É|Í|Ú])[\\s|A-Z|a-z|Ñ|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "El nombre del alumno debe comenzar con mayúscula")
+    String studentName;
+    String message;
+    Double averageScore;
+    @NotEmpty(message = "La lista no puede ser vacía")
+    List<SubjectDTO> subjects;
+}
---
+++ 5. testing/EJERCICIO_VALIDACIONES/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -0,0 +1,18 @@
+package com.meli.obtenerdiploma.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.*;
+
+@Getter @Setter
+public class SubjectDTO {
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
+    @Size(max = 30, message = "La longitud del nombre no puede superar los 50 caracteres")
+    @Pattern(regexp = "([A-Z|Ñ|Á|Ó|É|Í|Ú])[\\s|A-Z|a-z|Ñ|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "El nombre del alumno debe comenzar con mayúscula")
+    String name;
+    @NotNull(message = "La nota no puede estar vacía")
+    @Min(value = 0, message = "La mínima nota es 0.0")
+    @Max(value = 10, message = "La máxima nota es 10.0")
+    Double score;
+}
