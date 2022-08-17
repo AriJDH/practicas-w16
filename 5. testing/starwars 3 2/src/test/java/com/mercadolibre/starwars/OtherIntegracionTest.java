7807dbe4716ba3efc3cb88ce77b061b230fe13fd
emarivero1 <emanuel.rivero@mercadolibre.com>
2022-08-17 18:46:54 -0300

    add other exercies

--- 5. testing/starwars 3 2/src/test/java/com/mercadolibre/starwars/OtherIntegracionTest.java
+++ 5. testing/starwars 3 2/src/test/java/com/mercadolibre/starwars/OtherIntegracionTest.java
@@ -4,35 +4,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.mercadolibre.starwars.dto.CharacterDTO;
-import net.minidev.json.JSONObject;
-import org.apache.tomcat.util.json.JSONParser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @SpringBootTest
 @AutoConfigureMockMvc
-public class IntegrationTest {
+public class OtherIntegracionTest {
     @Autowired
     private MockMvc mockMvc;
-
     @Test
-    public void test() throws Exception{
+    public void withOtherFormTest() throws Exception{
         CharacterDTO characterDTO = new CharacterDTO();
         characterDTO.setName("Luke Skywalker");
         characterDTO.setHair_color("blond");
@@ -48,21 +40,15 @@ public class IntegrationTest {
         List<CharacterDTO> characterDTOList = new ArrayList<>();
         characterDTOList.add(characterDTO);
 
-        ObjectWriter writer = new ObjectMapper()
-                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
-                //withDefaultPrettyPrinter lo sacamos paara quitar la identacion.
-                .writer().withDefaultPrettyPrinter();
-        //String responseCharacterDTOListJson = writer.writeValueAsString(characterDTOList);
 
-        MvcResult mvcResult =
-                this.mockMvc.perform(MockMvcRequestBuilders.get("/{query}", "Luke"))
-                        .andDo(print()).andExpect(status().isOk())
-                        .andExpect(content().contentType("application/json"))
-                        .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Luke Skywalker"))
-                        .andReturn();
 
-        //assertEquals(responseCharacterDTOListJson, mvcResult.getResponse().getContentAsString());
-        assertEquals("application/json", mvcResult.getResponse().getContentType());
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
+        String responseJson = writer.writeValueAsString(characterDTOList);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/{query}", "Luke"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json(responseJson));
+
     }
-
 }
