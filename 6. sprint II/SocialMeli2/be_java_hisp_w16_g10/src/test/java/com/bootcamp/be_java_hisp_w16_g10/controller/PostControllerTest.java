c794ff27e68c7fc913a1271bdfb0618d4a1d5707
rubenBazanMeli <ruben.bazan@mercadolibre.com>
2022-08-18 15:21:06 -0300

    add test us005 postController

--- 6. sprint II/SocialMeli2/be_java_hisp_w16_g10/src/test/java/com/bootcamp/be_java_hisp_w16_g10/controller/PostControllerTest.java
+++ 6. sprint II/SocialMeli2/be_java_hisp_w16_g10/src/test/java/com/bootcamp/be_java_hisp_w16_g10/controller/PostControllerTest.java
@@ -1,12 +1,17 @@
 package com.bootcamp.be_java_hisp_w16_g10.controller;
 
+import com.bootcamp.be_java_hisp_w16_g10.dto.request.PostReqDTO;
+import com.bootcamp.be_java_hisp_w16_g10.dto.request.ProductReqDTO;
 import com.bootcamp.be_java_hisp_w16_g10.service.IPostService;
 import com.bootcamp.be_java_hisp_w16_g10.service.PostService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,7 +24,15 @@ class PostControllerTest {
     private PostController postController;
 
     @Test
+    @DisplayName("shouldSavePostReqDTO")
     void US005() {
+        ProductReqDTO productReqDTO = new ProductReqDTO(1, "Mac", "Compu", "Apple", "Gris","nada");
+        PostReqDTO postReqDTO = new PostReqDTO(1,1, LocalDate.now(),productReqDTO,10,1500.0);
+
+        var response = postController.US005(postReqDTO);
+
+        assertEquals(200, response.getStatusCodeValue());
+
     }
 
     @Test
