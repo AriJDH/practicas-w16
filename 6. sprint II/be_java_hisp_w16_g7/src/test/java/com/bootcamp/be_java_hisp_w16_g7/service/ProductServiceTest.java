f119a3eb66bd2db0f9350a4d10daa88a2ae9bc4b
marmejia <maria.mejia@mercadolibre.com.co>
2022-08-18 14:23:48 -0500

    Add test T-0008

--- 6. sprint II/be_java_hisp_w16_g7/src/test/java/com/bootcamp/be_java_hisp_w16_g7/service/ProductServiceTest.java
+++ 6. sprint II/be_java_hisp_w16_g7/src/test/java/com/bootcamp/be_java_hisp_w16_g7/service/ProductServiceTest.java
@@ -6,6 +6,8 @@ import com.bootcamp.be_java_hisp_w16_g7.dto.ResponsePostDTO;
 import com.bootcamp.be_java_hisp_w16_g7.entity.Post;
 import com.bootcamp.be_java_hisp_w16_g7.entity.Product;
 import com.bootcamp.be_java_hisp_w16_g7.entity.User;
+import com.bootcamp.be_java_hisp_w16_g7.exception.InvalidQueryException;
+import com.bootcamp.be_java_hisp_w16_g7.exception.UserNotFoundException;
 import com.bootcamp.be_java_hisp_w16_g7.repository.IUserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -130,6 +132,35 @@ class ProductServiceTest {
         //Assert
 
         Assertions.assertEquals(expected,response);
+    }
+
+    @Test
+    public void recentPostWithNonExistingUserTest(){
+
+        //Arrange
+        when(userRepository.existsUser(5555)).thenReturn(false);
+
+        //Act & Assert
+        assertThrows(UserNotFoundException.class, () -> productService.recentPost(5555, "date_desc" ));
+
+    }
+
+    @Test
+    public void recentPostWithWrongOrder(){
+
+        //Arrange
+        List<Post> posts = List.of(new Post(2222, 1234, LocalDate.of(2022, 8, 10), new Product(), 99, 1000.0, false, 0.0),
+                new Post(2222, 1234, LocalDate.of(2022, 7, 10), new Product(), 99, 1000.0, false, 0.0));
+        List<User> followers = List.of(new User(2222, "User2", List.of(), List.of(), posts ));
+        User userToMock = new User(1111, "User1", List.of(), followers, List.of());
+
+        when(userRepository.existsUser(1111)).thenReturn(true);
+        when(userRepository.findUserById(1111)).thenReturn(userToMock);
+
+
+        //Act & Assert
+        assertThrows(InvalidQueryException.class, () -> productService.recentPost(1111, "desc" ));
+
     }
 
 }
