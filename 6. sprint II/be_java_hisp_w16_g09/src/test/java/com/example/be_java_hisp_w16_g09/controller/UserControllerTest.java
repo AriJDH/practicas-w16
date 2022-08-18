4e1b483d71a534494486cac2af8b0d4a3d01a5a8
mnicoletta <maximiliano.nicoletta@mercadolibre.com>
2022-08-18 14:28:26 -0300

    add tests UserController T-0003

--- 6. sprint II/be_java_hisp_w16_g09/src/test/java/com/example/be_java_hisp_w16_g09/controller/UserControllerTest.java
+++ 6. sprint II/be_java_hisp_w16_g09/src/test/java/com/example/be_java_hisp_w16_g09/controller/UserControllerTest.java
@@ -1,13 +1,25 @@
 package com.example.be_java_hisp_w16_g09.controller;
 
+import com.example.be_java_hisp_w16_g09.dto.FollowersDtoResponse;
+import com.example.be_java_hisp_w16_g09.dto.SimpleUserDto;
+import com.example.be_java_hisp_w16_g09.model.User;
 import com.example.be_java_hisp_w16_g09.service.IPostService;
 import com.example.be_java_hisp_w16_g09.service.IUserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
     @Mock
     IUserService userService;
@@ -24,7 +36,34 @@ class UserControllerTest {
     }
 
     @Test
-    void US003() {
+    void US003DescTest() {
+        List<SimpleUserDto> list = List.of(new SimpleUserDto(3,"Mateo"),new SimpleUserDto(4,"Agustin"));
+        FollowersDtoResponse userMockResponse = new FollowersDtoResponse(2,"Marcos",list);
+        when(userService.orderByName(2,"name_desc")).thenReturn(userMockResponse);
+        ResponseEntity<FollowersDtoResponse> user = userController.US003(2,"name_desc");
+        String userName1 = user.getBody().getFollowers().get(0).getUserName();
+        String userName2= user.getBody().getFollowers().get(1).getUserName();
+        Assertions.assertTrue(userName1.compareTo(userName2)>0);
+    }
+    @Test
+    void US003AscTest() {
+        List<SimpleUserDto> list = List.of(new SimpleUserDto(4,"Agustin"),new SimpleUserDto(3,"Mateo"));
+        FollowersDtoResponse userMockResponse = new FollowersDtoResponse(2,"Marcos",list);
+        when(userService.orderByName(2,"name_asc")).thenReturn(userMockResponse);
+        ResponseEntity<FollowersDtoResponse> user = userController.US003(2,"name_asc");
+        String userName1 = user.getBody().getFollowers().get(0).getUserName();
+        String userName2= user.getBody().getFollowers().get(1).getUserName();
+        Assertions.assertTrue(userName1.compareTo(userName2)<0);
+    }
+    @Test
+    void US003Test() {
+        List<SimpleUserDto> list = List.of(new SimpleUserDto(4,"Agustin"),new SimpleUserDto(3,"Mateo"));
+        FollowersDtoResponse userMockResponse = new FollowersDtoResponse(2,"Marcos",list);
+        when(userService.getAllFollowers(2)).thenReturn(userMockResponse);
+        ResponseEntity<FollowersDtoResponse> user = userController.US003(2,null);
+        String userName1 = user.getBody().getFollowers().get(0).getUserName();
+        String userName2= user.getBody().getFollowers().get(1).getUserName();
+        Assertions.assertTrue(userName1.compareTo(userName2)<0);
     }
 
     @Test
