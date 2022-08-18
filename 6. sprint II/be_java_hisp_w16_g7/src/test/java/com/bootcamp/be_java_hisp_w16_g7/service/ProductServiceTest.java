fd7aa3d2515a8c0d0d279d8449e5a34ec319415a
juespitia <juan.espitia@mercadolibre.com.co>
2022-08-18 14:07:56 -0500

    Test T-0005 y T-0006 Realizados y esperando para revisar

--- 6. sprint II/be_java_hisp_w16_g7/src/test/java/com/bootcamp/be_java_hisp_w16_g7/service/ProductServiceTest.java
+++ 6. sprint II/be_java_hisp_w16_g7/src/test/java/com/bootcamp/be_java_hisp_w16_g7/service/ProductServiceTest.java
@@ -7,6 +7,7 @@ import com.bootcamp.be_java_hisp_w16_g7.entity.Post;
 import com.bootcamp.be_java_hisp_w16_g7.entity.Product;
 import com.bootcamp.be_java_hisp_w16_g7.entity.User;
 import com.bootcamp.be_java_hisp_w16_g7.repository.IUserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,6 +55,81 @@ class ProductServiceTest {
         RecentPostsDTO expected = new RecentPostsDTO(1111, List.of(expectedPost) );
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void orderByDateAscTest(){
+        //Arrange
+        Product product = new Product(1,"Moto","Vehiculo","WWW","Rojo","Nuevo");
+        Post post1 = new Post(2222,1,LocalDate.parse("2022-08-12"), product,11,12300,false,0);
+        Post post2 = new Post(2222,2,LocalDate.parse("2022-08-11"), product,11,11300,false,0);
+        User user2 = new User(2222,"Camilo",new ArrayList<>(),new ArrayList<>(),List.of(post1,post2));
+        User user1 = new User(1111,"Juan",new ArrayList<>(),List.of(user2),new ArrayList<>());
+
+        ProductDTO productDTO= new ProductDTO(1,"Moto","Vehiculo","WWW","Rojo","Nuevo");
+        ResponsePostDTO postDTO1 = new ResponsePostDTO(2222,1,LocalDate.parse("2022-08-12"), productDTO,11,12300);
+        ResponsePostDTO postDTO2 = new ResponsePostDTO(2222,2,LocalDate.parse("2022-08-11"), productDTO,11,11300);
+
+        RecentPostsDTO expected = new RecentPostsDTO(1111,List.of(postDTO2,postDTO1));
+
+        //Act
+        when(userRepository.existsUser(1111)).thenReturn(true);
+        when(userRepository.findUserById(1111)).thenReturn(user1);
+
+        RecentPostsDTO response = productService.recentPost(1111,"date_asc");
+        //Assert
+
+        Assertions.assertEquals(expected,response);
+    }
+
+    @Test
+    void orderByDateDesTest(){
+        //Arrange
+        Product product = new Product(1,"Moto","Vehiculo","WWW","Rojo","Nuevo");
+        Post post1 = new Post(2222,1,LocalDate.parse("2022-08-12"), product,11,12300,false,0);
+        Post post2 = new Post(2222,2,LocalDate.parse("2022-08-11"), product,11,11300,false,0);
+        User user2 = new User(2222,"Camilo",new ArrayList<>(),new ArrayList<>(),List.of(post1,post2));
+        User user1 = new User(1111,"Juan",new ArrayList<>(),List.of(user2),new ArrayList<>());
+
+        ProductDTO productDTO= new ProductDTO(1,"Moto","Vehiculo","WWW","Rojo","Nuevo");
+        ResponsePostDTO postDTO1 = new ResponsePostDTO(2222,1,LocalDate.parse("2022-08-12"), productDTO,11,12300);
+        ResponsePostDTO postDTO2 = new ResponsePostDTO(2222,2,LocalDate.parse("2022-08-11"), productDTO,11,11300);
+
+        RecentPostsDTO expected = new RecentPostsDTO(1111,List.of(postDTO1,postDTO2));
+
+        //Act
+        when(userRepository.existsUser(1111)).thenReturn(true);
+        when(userRepository.findUserById(1111)).thenReturn(user1);
+
+        RecentPostsDTO response = productService.recentPost(1111,"date_desc");
+        //Assert
+
+        Assertions.assertEquals(expected,response);
+    }
+
+    @Test
+    void orderByDateNullTest(){
+        //Arrange
+        Product product = new Product(1,"Moto","Vehiculo","WWW","Rojo","Nuevo");
+        Post post1 = new Post(2222,1,LocalDate.parse("2022-08-12"), product,11,12300,false,0);
+        Post post2 = new Post(2222,2,LocalDate.parse("2022-08-11"), product,11,11300,false,0);
+        User user2 = new User(2222,"Camilo",new ArrayList<>(),new ArrayList<>(),List.of(post1,post2));
+        User user1 = new User(1111,"Juan",new ArrayList<>(),List.of(user2),new ArrayList<>());
+
+        ProductDTO productDTO= new ProductDTO(1,"Moto","Vehiculo","WWW","Rojo","Nuevo");
+        ResponsePostDTO postDTO1 = new ResponsePostDTO(2222,1,LocalDate.parse("2022-08-12"), productDTO,11,12300);
+        ResponsePostDTO postDTO2 = new ResponsePostDTO(2222,2,LocalDate.parse("2022-08-11"), productDTO,11,11300);
+
+        RecentPostsDTO expected = new RecentPostsDTO(1111,List.of(postDTO1,postDTO2));
+
+        //Act
+        when(userRepository.existsUser(1111)).thenReturn(true);
+        when(userRepository.findUserById(1111)).thenReturn(user1);
+
+        RecentPostsDTO response = productService.recentPost(1111,null);
+        //Assert
+
+        Assertions.assertEquals(expected,response);
     }
 
 }
