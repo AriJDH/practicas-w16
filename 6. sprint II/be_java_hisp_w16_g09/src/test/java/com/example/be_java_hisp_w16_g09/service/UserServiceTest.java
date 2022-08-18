b6f2b64481d55302bcd96ff6d6a9b1543135d081
mnicoletta <maximiliano.nicoletta@mercadolibre.com>
2022-08-18 14:00:01 -0300

    add tests UserService T-0003

--- 6. sprint II/be_java_hisp_w16_g09/src/test/java/com/example/be_java_hisp_w16_g09/service/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w16_g09/src/test/java/com/example/be_java_hisp_w16_g09/service/UserServiceTest.java
@@ -1,22 +1,39 @@
 package com.example.be_java_hisp_w16_g09.service;
 
+import com.example.be_java_hisp_w16_g09.dto.FollowersDtoResponse;
+import com.example.be_java_hisp_w16_g09.dto.SimpleUserDto;
+import com.example.be_java_hisp_w16_g09.exception.OrderNotExist;
+import com.example.be_java_hisp_w16_g09.model.User;
 import com.example.be_java_hisp_w16_g09.repository.IPostRepository;
 import com.example.be_java_hisp_w16_g09.repository.IUserRepository;
+import com.example.be_java_hisp_w16_g09.repository.UserRepository;
+import com.example.be_java_hisp_w16_g09.utility.DTOMapperUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
     @Mock
     IUserRepository userRepository;
     @Mock
     IPostRepository postRepository;
+    @Mock
+    DTOMapperUtil dtoMapperUtil;
 
     @InjectMocks
-    IUserService userService;
+    UserService userService;
 
     @Test
     void followUser() {
@@ -35,8 +52,45 @@ class UserServiceTest {
     }
 
     @Test
-    void orderByName() {
+    void orderByNameAscTest() {
+        User userMock1= new User(3,"Mateo",null,null);
+        User userMock2= new User(4,"Agustin",null,null);
+        User userMock = new User(2,"Marcos", List.of(userMock1,userMock2),null);
+        when(userRepository.searchById(2)).thenReturn(userMock);
+        List<SimpleUserDto> list = new ArrayList<>();
+        list = List.of(new SimpleUserDto(3,"Mateo"),new SimpleUserDto(4,"Agustin"));
+        when(dtoMapperUtil.mapList(userMock.getFollowers(),SimpleUserDto.class)).thenReturn(list);
+        FollowersDtoResponse user = userService.orderByName(2,"name_asc");
+        String userName1 = user.getFollowers().get(0).getUserName();
+        String userName2= user.getFollowers().get(1).getUserName();
+        Assertions.assertTrue(userName1.compareTo(userName2)<0);
     }
+    @Test
+    void orderByNameDescTest() {
+        User userMock1= new User(3,"Agustin",null,null);
+        User userMock2= new User(4,"Mateo",null,null);
+        User userMock = new User(2,"Marcos", List.of(userMock1,userMock2),null);
+        when(userRepository.searchById(2)).thenReturn(userMock);
+        List<SimpleUserDto> list = new ArrayList<>();
+        list = List.of(new SimpleUserDto(3,"Agustin"),new SimpleUserDto(4,"Mateo"));
+        when(dtoMapperUtil.mapList(userMock.getFollowers(),SimpleUserDto.class)).thenReturn(list);
+        FollowersDtoResponse user = userService.orderByName(2,"name_desc");
+        String userName1 = user.getFollowers().get(0).getUserName();
+        String userName2= user.getFollowers().get(1).getUserName();
+        Assertions.assertTrue(userName1.compareTo(userName2)>0);
+    }
+    @Test
+    void orderByNameNotExistTest() {
+        User userMock1= new User(3,"Agustin",null,null);
+        User userMock2= new User(4,"Mateo",null,null);
+        User userMock = new User(2,"Marcos", List.of(userMock1,userMock2),null);
+        when(userRepository.searchById(2)).thenReturn(userMock);
+        List<SimpleUserDto> list = new ArrayList<>();
+        list = List.of(new SimpleUserDto(3,"Agustin"),new SimpleUserDto(4,"Mateo"));
+        when(dtoMapperUtil.mapList(userMock.getFollowers(),SimpleUserDto.class)).thenReturn(list);
+        Assertions.assertThrows(OrderNotExist.class,() -> userService.orderByName(2,"fdgdfg"));
+    }
+
 
     @Test
     void orderByNameFollowed() {
