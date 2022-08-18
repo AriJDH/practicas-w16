725ca74de4545f7a0e0c9bd78fa87368c38c8763
ser-cardenas <sergio.cardenas@mercadolibre.com.co>
2022-08-18 13:47:41 -0500

    repository existsUser tests

--- 6. sprint II/be_java_hisp_w16_g7/src/test/java/com/bootcamp/be_java_hisp_w16_g7/repository/UserRepositoryTest.java
+++ 6. sprint II/be_java_hisp_w16_g7/src/test/java/com/bootcamp/be_java_hisp_w16_g7/repository/UserRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.bootcamp.be_java_hisp_w16_g7.repository;
 
+import com.bootcamp.be_java_hisp_w16_g7.entity.User;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRepositoryTest {
@@ -9,4 +12,21 @@ class UserRepositoryTest {
 
     //Test
 
+    @Test
+    public void existsUserExistingUser() {
+        int id = 1111;
+
+        boolean response = userRepository.existsUser(id);
+
+        assertTrue(response);
+    }
+
+    @Test
+    public void existsUserNonexistentUser() {
+        int id = 9999;
+
+        boolean response = userRepository.existsUser(id);
+
+        assertFalse(response);
+    }
 }
