e814242d4a1074f2229c4c5187232fb9aa224784
javgomez <javier.gomez@mercadolibre.com>
2022-08-18 10:14:42 -0300

    fix coverage

--- 5. testing/ObtenerDiploma - Tests Integradores/src/main/java/com/meli/obtenerdiploma/model/ErrorDTO.java
+++ 5. testing/ObtenerDiploma - Tests Integradores/src/main/java/com/meli/obtenerdiploma/model/ErrorDTO.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class ErrorDTO {
     private String name;
     private String description;
