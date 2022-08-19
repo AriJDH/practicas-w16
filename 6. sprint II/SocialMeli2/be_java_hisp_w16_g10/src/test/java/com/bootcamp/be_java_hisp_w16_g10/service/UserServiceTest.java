eecf3c51e826f5cdfaa71454930e91bc7e4fcee0
rubenBazanMeli <ruben.bazan@mercadolibre.com>
2022-08-19 11:43:42 -0300

    add test validateUserNotFoundCountFollowers-shouldReturnCountFollowers-shouldReturnBadRequestExceptionInCountFollowers in userServiceTest

--- 6. sprint II/SocialMeli2/be_java_hisp_w16_g10/src/test/java/com/bootcamp/be_java_hisp_w16_g10/service/UserServiceTest.java
+++ 6. sprint II/SocialMeli2/be_java_hisp_w16_g10/src/test/java/com/bootcamp/be_java_hisp_w16_g10/service/UserServiceTest.java
@@ -1,5 +1,9 @@
 package com.bootcamp.be_java_hisp_w16_g10.service;
 
+import com.bootcamp.be_java_hisp_w16_g10.dto.response.FollowersCountResDTO;
+import com.bootcamp.be_java_hisp_w16_g10.entity.User;
+import com.bootcamp.be_java_hisp_w16_g10.exception.BadRequestException;
+import com.bootcamp.be_java_hisp_w16_g10.exception.NotFoundException;
 import com.bootcamp.be_java_hisp_w16_g10.repository.PostRepository;
 import com.bootcamp.be_java_hisp_w16_g10.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -8,7 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+
+import static com.bootcamp.be_java_hisp_w16_g10.util.Factory.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -35,7 +45,40 @@ class UserServiceTest {
     }
 
     @Test
-    void countFollowers() {
+    void validateUserNotFoundCountFollowers() {
+        //assert
+        assertThrows(NotFoundException.class, () -> {
+            when(userService.countFollowers(anyInt())).thenReturn(null);
+        });
+    }
+
+    @Test
+    void shouldReturnCountFollowers() {
+        //arrange
+        var user = generateUser(1);
+        //act
+
+        when(userRepository.findById(1)).thenReturn(user);
+        when(postService.findByUserId(anyInt())).thenReturn(generateListPostResDTO(4,1));
+
+        var res = userService.countFollowers(1);
+
+        //assert
+        assertEquals(0,res.getFollowersCount());
+
+    }
+
+    @Test
+    void shouldReturnBadRequestExceptionInCountFollowers() {
+        //arrange
+        var user = generateUser(1);
+
+        //act
+        when(userRepository.findById(1)).thenReturn(user);
+        when(postService.findByUserId(anyInt())).thenReturn(generateListPostResDTO(0,1));
+
+        //assert
+        assertThrows(BadRequestException.class, () -> userService.countFollowers(1));
     }
 
     @Test
