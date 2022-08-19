41699ae6287af6d46593622df63cf50b28db2a6b
gamoralesv <Gab0o.1996>
2022-08-19 14:07:53 -0400

    Proyecto Social Meli 2

---
+++ 6. sprint II/SocialMeli2/be_java_hisp_w16_g01/src/main/java/com/bootcamp/be_java_hisp_w16_g01/config/SwaggerConfiguration.java
@@ -0,0 +1,21 @@
+package com.bootcamp.be_java_hisp_w16_g01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
