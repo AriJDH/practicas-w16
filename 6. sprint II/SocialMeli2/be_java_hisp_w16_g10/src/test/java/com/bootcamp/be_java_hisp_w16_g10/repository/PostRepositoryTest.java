62682fd96688337b7f86b8331c35d69b933a3451
Matias Lamela <matias.lamela@mercadolibre.com>
2022-08-19 11:46:32 -0300

    adding findById test of the userRepository

--- 6. sprint II/SocialMeli2/be_java_hisp_w16_g10/src/test/java/com/bootcamp/be_java_hisp_w16_g10/repository/PostRepositoryTest.java
+++ 6. sprint II/SocialMeli2/be_java_hisp_w16_g10/src/test/java/com/bootcamp/be_java_hisp_w16_g10/repository/PostRepositoryTest.java
@@ -4,30 +4,37 @@ import com.bootcamp.be_java_hisp_w16_g10.entity.Post;
 import com.bootcamp.be_java_hisp_w16_g10.util.Factory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostRepositoryTest {
-
     private PostRepository postRepository;
+    private List<Post> posts;
 
     @BeforeEach()
     public void init() {
         this.postRepository = new PostRepository();
-        List<Post> posts = Factory.generateListOfPosts(3, 1);
-        posts.forEach(post -> this.postRepository.save(post));
+        this.posts = Factory.generateListOfPosts(3, 1);
+        this.posts.forEach(post -> this.postRepository.save(post));
     }
     @Test
     void findAll() {
     }
-
     @Test
     void givenAnIdShouldReturnAPostWhenIsCalled() {
-        assertNotNull(this.postRepository.findById(1));
+        Post post = this.postRepository.findById(1);
+        assertNotNull(post);
+        assertInstanceOf(Post.class, post);
     }
-
+    @Test
+    void givenANonExistentIdShouldReturnNullWhenIsCalled() {
+        Post post = this.postRepository.findById(321321);
+        assertNull(post);
+    }
     @Test
     void save() {
     }
