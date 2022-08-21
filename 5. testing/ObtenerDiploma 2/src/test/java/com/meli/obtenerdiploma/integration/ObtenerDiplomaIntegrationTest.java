56701de4ca527c8392bba6806068a823e9d7ae43
Ivana Belen Coronel Aguirre <ivana.coronel@mercadolibre.com>
2022-08-20 22:51:50 -0300

    integration test

---
+++ 5. testing/ObtenerDiploma 2/src/test/java/com/meli/obtenerdiploma/integration/ObtenerDiplomaIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.meli.obtenerdiploma.integration;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ObtenerDiplomaIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void analizeScoreTest() throws Exception {
+        List<SubjectDTO> subjects = new ArrayList<>();
+        subjects.add(new SubjectDTO("Matemática", 9.0));
+        subjects.add(new SubjectDTO("Física", 7.0));
+        subjects.add(new SubjectDTO("Química", 6.0));
+        Double average = subjects.stream()
+                .reduce(0D, (partialSum, score)  -> partialSum + score.getScore(), Double::sum)
+                / subjects.size();
+
+        String message = "El alumno Juan ha obtenido un promedio de " + new DecimalFormat("#0.00").format(average) +". Puedes mejorar.";
+        StudentDTO studentExpect = new StudentDTO(1L, "Juan", message, average, subjects);
+
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer();
+        String expectJson = writer.writeValueAsString(studentExpect);
+
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/analyzeScores/{studentId}", 1L))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        Assertions.assertEquals(expectJson,mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8));
+    }
+
+
+}
