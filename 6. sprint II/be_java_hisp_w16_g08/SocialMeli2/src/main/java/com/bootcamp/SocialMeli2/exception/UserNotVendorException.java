828e6112ed324dfafdef5bf0aa662f8271035757
jobracco <jonathan.bracco@mercadolibre.com>
2022-08-22 12:52:11 -0300

    Se soluciona problema con el trabajo grupal

---
+++ 6. sprint II/be_java_hisp_w16_g08/SocialMeli2/src/main/java/com/bootcamp/SocialMeli2/exception/UserNotVendorException.java
@@ -0,0 +1,15 @@
+package com.bootcamp.SocialMeli2.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class UserNotVendorException extends RuntimeException{
+
+    private final HttpStatus htttpStatus;
+    private final String exception;
+
+    public UserNotVendorException() {
+        super("User not vendor");
+        this.htttpStatus = HttpStatus.BAD_REQUEST;
+        this.exception = "Bad request";
+    }
+}
