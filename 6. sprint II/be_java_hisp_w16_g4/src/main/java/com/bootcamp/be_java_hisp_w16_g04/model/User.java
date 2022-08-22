40b5dd49d44595c18872f777b2aee56d86487863
KevGamarra <kevin.molano@mercadolibre.com.co>
2022-08-22 10:30:30 -0500

    Adding group project

---
+++ 6. sprint II/be_java_hisp_w16_g4/src/main/java/com/bootcamp/be_java_hisp_w16_g04/model/User.java
@@ -0,0 +1,23 @@
+package com.bootcamp.be_java_hisp_w16_g04.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class User {
+
+  private Integer userId;
+  private String userName;
+}
---
+++ 6. sprint II/be_java_hisp_w16_g4/src/main/java/com/bootcamp/be_java_hisp_w16_g04/service/PublicationService.java
@@ -0,0 +1,119 @@
+package com.bootcamp.be_java_hisp_w16_g04.service;
+
+import com.bootcamp.be_java_hisp_w16_g04.dto.ListProductByDateDTO;
+import com.bootcamp.be_java_hisp_w16_g04.dto.PostDTO;
+import com.bootcamp.be_java_hisp_w16_g04.exception.OrderNotFoundException;
+import com.bootcamp.be_java_hisp_w16_g04.model.Publication;
+import com.bootcamp.be_java_hisp_w16_g04.model.User;
+import com.bootcamp.be_java_hisp_w16_g04.repositories.IPublicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.bootcamp.be_java_hisp_w16_g04.dto.PublicationDTO;
+import com.bootcamp.be_java_hisp_w16_g04.dto.RequestCreatePublicationDTO;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Service that takes care of everything related to publishing
+ */
+@Service
+public class PublicationService implements IPublicationService {
+
+  @Autowired
+  IPublicationRepository iPublicationRepository;
+  @Autowired
+  IUserService iUserService;
+  @Autowired
+  IProductService iProductService;
+
+  private final static String DATE_DESC = "date_desc";
+  private final static String DATE_ASC = "date_asc";
+
+  /**
+   * Method that returns the publications of the people I follow in date order.
+   *
+   * @param userId Current user id
+   * @param order  Order of the list (ascending or descending)
+   * @return DTO of a list of publications
+   */
+  @Override
+  public ListProductByDateDTO getListProductByDate(Integer userId, String order) {
+
+    if(!order.equals(DATE_ASC) && !order.equals(DATE_DESC)){
+      throw new OrderNotFoundException("Order does not exist");
+    }
+
+    iUserService.isValidUser(userId);
+
+    List<User> users = iUserService.orderListUserFollowed(userId, "").getFollowed();
+    List<Publication> sellers = getListSeller(users);
+    List<PostDTO> listDTO = listOrderByWeekend(sellers);
+    if (order.equals(DATE_DESC)) {
+      listDTO = listDTO.stream().sorted(Comparator.comparing(PostDTO::getDate).reversed()).collect(Collectors.toList());
+    }
+    return new ListProductByDateDTO(userId, listDTO);
+  }
+
+  /**
+   * Method to obtain the list of publications of the people I follow.
+   *
+   * @param users List of users followed by the current user
+   * @return List with the publications of the users I follow
+   */
+  private List<Publication> getListSeller(List<User> users) {
+    List<Publication> sellers = new ArrayList<>();
+    for (User user : users) {
+      List<Publication> publications = iPublicationRepository.getListPublicationsById(user.getUserId());
+      if (publications != null && publications.size() > 0) {
+        sellers.addAll(publications);
+      }
+    }
+    return sellers;
+  }
+
+  /**
+   * Method that filters the list of products and obtains the products of the last two weeks.
+   *
+   * @param publications List of publications of people followed by the current user
+   * @return List of publications for the last two weeks
+   */
+  private List<PostDTO> listOrderByWeekend(List<Publication> publications) {
+    LocalDate date = LocalDate.now().minusDays(15);
+
+    List<PostDTO> result = publications.stream()
+            .filter(x -> x.getDate().isAfter(date))
+            .map(p -> new PostDTO(p.getUserId(), p.getPublicationId(),
+                    p.getDate(), iProductService.getProductById(p.getProductId()), p.getCategory(), p.getPrice()))
+            .sorted(Comparator.comparing(PostDTO::getDate))
+            .collect(Collectors.toList());
+
+    return result;
+  }
+
+
+  /**
+   * Method for creating a publication
+   *
+   * @param requestCreatePublicationDTO DTO of a publication that is sent from request
+   * @return Boolean that checks if the publication was created
+   */
+  @Override
+  public Boolean createPublication(RequestCreatePublicationDTO requestCreatePublicationDTO) {
+
+    iUserService.isValidUser(requestCreatePublicationDTO.getUserId());
+
+    PublicationDTO publicationDTO = new PublicationDTO(requestCreatePublicationDTO.getUserId(),
+            requestCreatePublicationDTO.getDate(),
+            requestCreatePublicationDTO.getCategory(),
+            requestCreatePublicationDTO.getPrice(),
+            requestCreatePublicationDTO.getProduct().getProductId());
+
+    Publication publication = iPublicationRepository.createPublication(publicationDTO);
+
+    return publication != null;
+  }
+}
