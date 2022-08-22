dc1afcc966b557c5cdce310444b16521c01b92f7
javier-antonio02 <javier.aguayo@mercadolibre.cl>
2022-08-22 15:14:37 -0400

    Delete project error

--- 6. sprint II/SocialMeli2/be_java_hisp_w16_g04/src/main/java/com/bootcamp/be_java_hisp_w16_g04/exception/OrderNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.be_java_hisp_w16_g04.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
