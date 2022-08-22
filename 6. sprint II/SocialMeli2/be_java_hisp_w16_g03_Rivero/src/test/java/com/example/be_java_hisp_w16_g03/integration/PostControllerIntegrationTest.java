c68135d9af2f3098aeb8d4bdc54eb8ee361bdf01
emarivero1 <emanuel.rivero@mercadolibre.com>
2022-08-22 15:20:59 -0300

    94% coverage

--- 6. sprint II/SocialMeli2/be_java_hisp_w16_g03_Rivero/src/test/java/com/example/be_java_hisp_w16_g03/integration/PostControllerIntegrationTest.java
+++ 6. sprint II/SocialMeli2/be_java_hisp_w16_g03_Rivero/src/test/java/com/example/be_java_hisp_w16_g03/integration/PostControllerIntegrationTest.java
@@ -1,9 +1,8 @@
 package com.example.be_java_hisp_w16_g03.integration;
 
-import com.example.be_java_hisp_w16_g03.dto.PostDTO;
-import com.example.be_java_hisp_w16_g03.dto.PostWithIdDTO;
-import com.example.be_java_hisp_w16_g03.dto.PostsDTO;
-import com.example.be_java_hisp_w16_g03.dto.ProductDTO;
+import com.example.be_java_hisp_w16_g03.dto.*;
+import com.example.be_java_hisp_w16_g03.entity.Post;
+import com.example.be_java_hisp_w16_g03.entity.Product;
 import com.example.be_java_hisp_w16_g03.utils.Mapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -13,22 +12,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PostControllerIntegrationTest {
@@ -37,13 +34,14 @@ public class PostControllerIntegrationTest {
     MockMvc mockMvc;
 
     @Test
-    public void addPost() throws  Exception{
-        PostDTO postDTO=new PostDTO();
+    public void addPost() throws Exception {
+        PostDTO postDTO = new PostDTO();
         postDTO.setUserId(5);
-        postDTO.setDate(LocalDate.of(2022,8,14));
+        postDTO.setDate(LocalDate.of(2022, 8, 14));
         postDTO.setCategory(4);
         postDTO.setPrice(2140d);
-        postDTO.setProduct(new ProductDTO(1,"Cabeza","Extremo","Maximo","Rojo","vacio"));
+        postDTO.setProduct(new ProductDTO(1, "Cabeza", "Extremo"
+                , "Maximo", "Rojo", "vacio"));
 
         ObjectWriter writer = new ObjectMapper()
                 .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
@@ -56,23 +54,19 @@ public class PostControllerIntegrationTest {
                 .andDo(print()).andExpect(status().isOk());
 
     }
-    /*@GetMapping("/products/followed/{userId}/list")
-    public ResponseEntity<PostsDTO> getLatestPostsOrderedByUserId(@PathVariable Integer userId, @RequestParam(required = false) String order) {
-        return new ResponseEntity<>(service.getLatestPostsOrderedByUserId(userId, order), HttpStatus.OK);
-    }*/
+
     @Test
-    public void getLatestPostsOrderedASCByUserIdTest() throws Exception{
-        PostDTO postDTO=new PostDTO();
+    public void getLatestPostsOrderedASCByUserIdTest() throws Exception {
+        PostDTO postDTO = new PostDTO();
         postDTO.setUserId(5);
-        postDTO.setDate(LocalDate.of(2022,8,14));
+        postDTO.setDate(LocalDate.of(2022, 8, 14));
         postDTO.setCategory(4);
         postDTO.setPrice(2140d);
-        postDTO.setProduct(new ProductDTO(1,"Cabeza","Extremo"
-                ,"Maximo","Rojo","vacio"));
-        List<PostWithIdDTO> listPosts=Arrays.asList(Mapper.postToPostWithIdDto(Mapper.postDtoToEntity(postDTO)));
+        postDTO.setProduct(new ProductDTO(1, "Cabeza", "Extremo"
+                , "Maximo", "Rojo", "vacio"));
+        List<PostWithIdDTO> listPosts = Arrays.asList(Mapper.postToPostWithIdDto(Mapper.postDtoToEntity(postDTO)));
         listPosts.get(0).setPostId(0);
-        List<PostDTO> listPost=Arrays.asList(postDTO);
-        PostsDTO postsDTO=new PostsDTO();
+        PostsDTO postsDTO = new PostsDTO();
         postsDTO.setUserId(1);
         postsDTO.setPosts(listPosts);
 
@@ -84,12 +78,79 @@ public class PostControllerIntegrationTest {
         String responseJson = writer.writeValueAsString(postsDTO);
 
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/products/followed/"+1+"/list"+"?order=date_desc"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/products/followed/" + 1
+                        + "/list" + "?order=date_asc"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(content().json(responseJson));
     }
 
+    @Test
+    public void getLatestPostsOrderedDESCByUserIdTest() throws Exception {
+        Post post = new Post();
+        post.setUserId(5);
+        post.setDate(LocalDate.of(2022, 8, 14));
+        post.setCategory(4);
+        post.setPrice(2140d);
+        post.setProduct(new Product(1, "Extremo", "Maximo"
+                , "Rojo", "vacio", "Cabeza"));
+        List<PostWithIdDTO> listPosts = Arrays.asList(Mapper.postToPostWithIdDto(post));
+        listPosts.get(0).setPostId(0);
+        PostsDTO postsDTO = new PostsDTO();
+        postsDTO.setUserId(1);
+        postsDTO.setPosts(listPosts);
+
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .registerModule(new JavaTimeModule())
+                .writer().withDefaultPrettyPrinter();
+        String responseJson = writer.writeValueAsString(postsDTO);
+
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/products/followed/" + 1 +
+                        "/list" + "?order=date_desc"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json(responseJson));
+    }
+
+    @Test
+    public void getErrorForValidation() throws Exception {
+        PostDTO postDTO = new PostDTO();
+        postDTO.setUserId(5);
+        postDTO.setDate(LocalDate.of(2022, 8, 14));
+        postDTO.setCategory(4);
+        postDTO.setPrice(2140d);
+        postDTO.setProduct(new ProductDTO(1, "Cabeza", "Extremo"
+                , "Maximo", "Rojo y azul", "vacio"));
+
+        HashMap<String, List<String>> errors = new HashMap<>();
+        List<String> listaRes = new ArrayList<>();
+        listaRes.add("El campo no puede poseer caracteres especiales");
+        errors.put("product.color", listaRes);
+        ExceptionApiDTO exceptionApiDTO = new ExceptionApiDTO();
+        exceptionApiDTO.setMessage("There are some fields that don't respect validations");
+        exceptionApiDTO.setTitle("Payload's Field Not Valid Exception");
+        exceptionApiDTO.setErrors(errors);
+
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .registerModule(new JavaTimeModule())
+                .writer().withDefaultPrettyPrinter();
+
+
+        String payloadjson = writer.writeValueAsString(postDTO);
+        String responsejson = writer.writeValueAsString(exceptionApiDTO);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/products/post")
+                        .contentType(MediaType.APPLICATION_JSON).content(payloadjson))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(content().json(responsejson));
+
+
+    }
 
 
 }
--- 6. sprint II/SocialMeli2/be_java_hisp_w16_g03_Rivero/src/test/java/com/example/be_java_hisp_w16_g03/integration/UserControllerIntegrationTests.java
+++ 6. sprint II/SocialMeli2/be_java_hisp_w16_g03_Rivero/src/test/java/com/example/be_java_hisp_w16_g03/integration/UserControllerIntegrationTests.java
@@ -1,8 +1,6 @@
 package com.example.be_java_hisp_w16_g03.integration;
 
 import com.example.be_java_hisp_w16_g03.dto.*;
-import com.example.be_java_hisp_w16_g03.entity.Post;
-import com.example.be_java_hisp_w16_g03.utils.Mapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -11,18 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import static com.example.be_java_hisp_w16_g03.utils.MocksPost.*;
-import static com.example.be_java_hisp_w16_g03.utils.MocksUser.createSeller;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -38,19 +28,20 @@ public class UserControllerIntegrationTests {
     MockMvc mockMvc;
 
     @Test
-    public void createUserTest() throws  Exception{
-        UserDTO userDTO1=new UserDTO(7,"jorge");
-        UserDTO userDTO2=new UserDTO(8,"pedro");
-        UserDTO userDTO3=new UserDTO(9,"felipe");
-        List<UserDTO> listUserDTO= Arrays.asList(userDTO1,userDTO2,userDTO3);
-        UserDTO user1 = new UserDTO (1,"pablo");
-        UserDTO user2 = new UserDTO (2,"emanuel");
-        UserDTO user3 = new UserDTO (3,"joaco");
-        UserDTO user4 = new UserDTO (4,"lucas");
-        UserDTO user5 = new UserDTO (5,"agustin");
-        UserDTO user6 = new UserDTO (6,"ivana");
-        List<UserDTO> response=Arrays.asList(user1,user2,user3,user4,user5,user6,userDTO1,userDTO2,userDTO3);
-        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).registerModule(new JavaTimeModule()).writer().withDefaultPrettyPrinter();
+    public void createUserTest() throws Exception {
+        UserDTO userDTO1 = new UserDTO(7, "jorge");
+        UserDTO userDTO2 = new UserDTO(8, "pedro");
+        UserDTO userDTO3 = new UserDTO(9, "felipe");
+        List<UserDTO> listUserDTO = Arrays.asList(userDTO1, userDTO2, userDTO3);
+        UserDTO user1 = new UserDTO(1, "pablo");
+        UserDTO user2 = new UserDTO(2, "emanuel");
+        UserDTO user3 = new UserDTO(3, "joaco");
+        UserDTO user4 = new UserDTO(4, "lucas");
+        UserDTO user5 = new UserDTO(5, "agustin");
+        UserDTO user6 = new UserDTO(6, "ivana");
+        List<UserDTO> response = Arrays.asList(user1, user2, user3, user4, user5, user6, userDTO1, userDTO2, userDTO3);
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .registerModule(new JavaTimeModule()).writer().withDefaultPrettyPrinter();
 
         String payloadjson = writer.writeValueAsString(listUserDTO);
         String responseJson = writer.writeValueAsString(response);
@@ -62,14 +53,16 @@ public class UserControllerIntegrationTests {
                 .andExpect(content().json(responseJson));
 
     }
-    @Test
-    public void createUserExistsErrorTest() throws  Exception{
-        UserDTO userDTO1=new UserDTO(1,"jorge");
-        UserDTO userDTO2=new UserDTO(2,"pedro");
-        UserDTO userDTO3=new UserDTO(3,"felipe");
-        List<UserDTO> listUserDTO= Arrays.asList(userDTO1,userDTO2,userDTO3);
 
-        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
+    @Test
+    public void createUserExistsErrorTest() throws Exception {
+        UserDTO userDTO1 = new UserDTO(1, "jorge");
+        UserDTO userDTO2 = new UserDTO(2, "pedro");
+        UserDTO userDTO3 = new UserDTO(3, "felipe");
+        List<UserDTO> listUserDTO = Arrays.asList(userDTO1, userDTO2, userDTO3);
+
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer().withDefaultPrettyPrinter();
         String payloadjson = writer.writeValueAsString(listUserDTO);
         //String responseJson = writer.writeValueAsString(response);
 
@@ -84,37 +77,41 @@ public class UserControllerIntegrationTests {
     @Test
     public void getCountFollowers() throws Exception {
 
-        FollowerCountDTO response= new FollowerCountDTO();
+        FollowerCountDTO response = new FollowerCountDTO();
         response.setFollowersCount(0);
         response.setUserId(1);
         response.setUserName("pablo");
 
-        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer().withDefaultPrettyPrinter();
         String responseJson = writer.writeValueAsString(response);
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/"+1+"/followers/count"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/" + 1 + "/followers/count"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(content().json(responseJson));
     }
+
     @Test
     public void followUserErrorAlreadyFollowersTest() throws Exception {
 
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/"+1+"/follow/"+6))
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/" + 1 + "/follow/" + 6))
                 .andDo(print()).andExpect(status().isBadRequest())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(result -> result.getResolvedException());
 
     }
+
     @Test
     public void followUserErrorAlreadyFollowersExceptionTest() throws Exception {
-        ExceptionApiDTO exceptionApiDTO=new ExceptionApiDTO();
+        ExceptionApiDTO exceptionApiDTO = new ExceptionApiDTO();
         exceptionApiDTO.setTitle("Error");
         exceptionApiDTO.setMessage("El usuario con el id: 1 ya es seguidor de : 6");
 
-        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer().withDefaultPrettyPrinter();
         String responseJson = writer.writeValueAsString(exceptionApiDTO);
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/"+1+"/follow/"+6))
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/" + 1 + "/follow/" + 6))
                 .andDo(print()).andExpect(status().isBadRequest())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(content().json(responseJson));
@@ -124,7 +121,7 @@ public class UserControllerIntegrationTests {
     @Test
     public void followUserIsNotSellerTest() throws Exception {
 
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/"+1+"/follow/"+3))
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/" + 1 + "/follow/" + 3))
                 .andDo(print()).andExpect(status().isBadRequest())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(result -> result.getResolvedException());
@@ -132,35 +129,34 @@ public class UserControllerIntegrationTests {
     }
 
     @Test
-    public void unfollowUserNotExistTest() throws  Exception{
+    public void unfollowUserNotExistTest() throws Exception {
 
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/"+2+"/unfollow/"+5))
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/" + 2 + "/unfollow/" + 5))
                 .andDo(print()).andExpect(status().isBadRequest())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(result -> result.getResolvedException());
     }
-    @Test
-    public void unfollowUserExistTest() throws  Exception{
 
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/"+1+"/unfollow/"+2))
+    @Test
+    public void unfollowUserExistTest() throws Exception {
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/" + 1 + "/unfollow/" + 2))
                 .andDo(print()).andExpect(status().isOk());
     }
-   /* @GetMapping("/users/{userId}/followed/list")
-    public ResponseEntity<FollowedsDTO> getFollowedUsers(@PathVariable Integer userId, @RequestParam(required = false) String order) {
-        return new ResponseEntity<>(service.getFollowedUsers(userId, order), HttpStatus.OK);
-    }*/
+
 
     @Test
-    public void getFollowedUserASCTest()throws Exception{
+    public void getFollowedUserASCTest() throws Exception {
 
-        FollowedsDTO followedsDTO=new FollowedsDTO();
+        FollowedsDTO followedsDTO = new FollowedsDTO();
         followedsDTO.setFollowed(new ArrayList<>());
         followedsDTO.setUserName("agustin");
         followedsDTO.setUserId(5);
-        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer().withDefaultPrettyPrinter();
         String responseJson = writer.writeValueAsString(followedsDTO);
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/"+5+"/followed/list?order=name_desc"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/" + 5 + "/followed/list?order=name_desc"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(content().json(responseJson));
@@ -168,16 +164,17 @@ public class UserControllerIntegrationTests {
 
 
     @Test
-    public void getFollowersUserASCTest()throws Exception{
+    public void getFollowersUserASCTest() throws Exception {
 
-        FollowersDTO followersDTO=new FollowersDTO();
+        FollowersDTO followersDTO = new FollowersDTO();
         followersDTO.setFollowers(new ArrayList<>());
         followersDTO.setUserName("agustin");
         followersDTO.setUserId(5);
-        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
+        ObjectWriter writer = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer().withDefaultPrettyPrinter();
         String responseJson = writer.writeValueAsString(followersDTO);
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/"+5+"/followers/list?order=name_desc"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/" + 5 + "/followers/list?order=name_desc"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(content().json(responseJson));
