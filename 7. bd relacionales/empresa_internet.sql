519d52a37d1ce71481613dc0ace0465d6b443fef
Joaquin Antonio Caetano <jocaetano@uy0fvfhk2htq05p.ml.com>
2022-08-24 14:32:59 -0300

    add empresaInternet.sql

---
+++ 7. bd relacionales/empresa_internet.sql
@@ -0,0 +1,29 @@
+USE empresa_internet;
+
+INSERT INTO Planes VALUE(1,50,1500.00,0.25);
+INSERT INTO Planes VALUE(2,100,2000.00,null);
+INSERT INTO Planes VALUE(3,200,4000.00,0.5);
+INSERT INTO Planes VALUE(4,500,5500.00,0.2);
+INSERT INTO Planes VALUE(5,1000,8499.99,0.10);
+
+INSERT INTO Clientes VALUE(101,'Name1','Sur1','1995-10-01','Buenos Aires','Buenos Aires',3);
+INSERT INTO Clientes VALUE(102,'Name2','Sur2','1995-10-30','Buenos Aires','Buenos Aires',2);
+INSERT INTO Clientes VALUE(103,'Name3','Sur3','1997-10-01','Buenos Aires','Buenos Aires',1);
+INSERT INTO Clientes VALUE(104,'Name4','Sur4','1998-02-18','Buenos Aires','Buenos Aires',1);
+INSERT INTO Clientes VALUE(105,'Name5','Sur5','1999-10-01','Buenos Aires','Buenos Aires',4);
+INSERT INTO Clientes VALUE(106,'Name6','Sur6','1991-10-01','Buenos Aires','Buenos Aires',4);
+INSERT INTO Clientes VALUE(107,'Name7','Sur7','1990-10-01','Buenos Aires','Buenos Aires',5);
+INSERT INTO Clientes VALUE(108,'Name8','Sur8','1993-10-10','Buenos Aires','Buenos Aires',5);
+INSERT INTO Clientes VALUE(109,'Name9','Sur9','1993-12-01','Buenos Aires','Buenos Aires',1);
+INSERT INTO Clientes VALUE(110,'Name10','Sur10','1992-10-01','Buenos Aires','Buenos Aires',1);
+
+####### CONSULTAS
+SELECT * FROM Planes;
+SELECT * FROM Clientes;
+SELECT * FROM Planes WHERE descuento <= 0.5;
+SELECT * FROM Planes ORDER BY precio DESC;
+SELECT * FROM Clientes WHERE Planes_idPlan = 5;
+SELECT dni, nombre, apellido, fecha_nacimiento WHERE fecha_nacimiento = '1995-10-01';
+SELECT * FROM Planes ORDER BY precio DESC LIMIT 3;
+SELECT Planes_idPlan, COUNT(*) AS NumInscriptos FROM Clientes WHERE Planes_idPlan = 1;
+SELECT MAX(precio) as MaxPrecio FROM Planes;
