42891a92b6d93d6886de6f55e5e7aef9c3d76108
Joaquin Antonio Caetano <jocaetano@uy0fvfhk2htq05p.ml.com>
2022-08-26 16:06:21 -0300

    add practico bd 2

--- 7. bd relacionales/moduloAsincronico1.sql
+++ 7. bd relacionales/moduloAsincronico1.sql
@@ -1,32 +1,49 @@
-/*Mostrar el título y el nombre del género de todas las series.
-Mostrar el título de los episodios, el nombre y apellido de los actores que trabajan en cada uno de ellos.
-Mostrar el título de todas las series y el total de temporadas que tiene cada una de ellas.
-Mostrar el nombre de todos los géneros y la cantidad total de películas por cada uno, siempre que sea mayor o igual a 3.
-Mostrar sólo el nombre y apellido de los actores que trabajan en todas las películas de la guerra de las galaxias y que estos no se repitan.
+/*Resolver las siguientes consignas
+Tomando la base de datos movies_db.sql, se solicita:
+Agregar una película a la tabla movies.
+Agregar un género a la tabla genres.
+Asociar a la película del punto 1. con el género creado en el punto 2.
+Modificar la tabla actors para que al menos un actor tenga como favorita la película agregada en el punto 1.
+Crear una tabla temporal copia de la tabla movies.
+Eliminar de esa tabla temporal todas las películas que hayan ganado menos de 5 awards.
+Obtener la lista de todos los géneros que tengan al menos una película.
+Obtener la lista de actores cuya película favorita haya ganado más de 3 awards.
+Crear un índice sobre el nombre en la tabla movies.
+Chequee que el índice fue creado correctamente.
+En la base de datos movies ¿Existiría una mejora notable al crear índices? Analizar y justificar la respuesta.
+¿En qué otra tabla crearía un índice y por qué? Justificar la respuesta
 */
 
 
 
 
-select s.title, g.name  from genres g inner join series s 
-on (g.id = s.genre_id)
-
-select e.title,a.first_name, a.last_name from episodes e inner join actor_episode ae
-on(e.id = ae.episode_id)
-left join actors a 
-on(a.id = ae.actor_id)
-
-select count(*) as total_temporadas , s.title  from series s left join seasons s2 ON (s.id=s2.serie_id) group by s.title 
+INSERT INTO movies_db.movies
+(created_at, updated_at, title, rating, awards, release_date, `length`, genre_id)
+VALUES('', '', '', 0, 0, '', 0, 1);
 
 
-select g.name , count(m.id) from genres g left join movies m  on (g.id = m.genre_id)
-group by g.name 
-having count(m.id) > 3 
 
-select * from movies m 
+INSERT INTO movies_db.genres
+(created_at, updated_at, name, ranking, active)
+VALUES('', '', '', 0, 1);
 
 
-select  distinct a.first_name, a.last_name from actor_movie am left join actors a 
-on(a.id = am.id)
-left join movies m on (m.id = am.movie_id)
-where m.title like '%galaxias%'
+
+create temporary table movies_copy(
+id int, created_at timestamp , updated_at timestamp,tittle varchar(500),
+raiting decimal(3,1) , awards int , release_date datetime , lenght int , genre_id int)
+
+
+delete from movies where awards < 5
+
+
+select distinct(name) from genres g  left join movies m 
+on(g.id=m.genre_id)
+
+select * from actors a left join movies m on
+(a.favorite_movie_id = m.id) where 
+m.awards > 3
+
+
+CREATE INDEX movies_id_IDX USING BTREE ON movies_db.movies (id,release_date);
+
