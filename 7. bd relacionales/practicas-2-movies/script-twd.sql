d508e4a9ef0e0928907a0d99e02a8590d9145fdb
Gianni <gianni.tiezzi@mercadolibre.com>
2022-08-26 17:05:51 -0300

    Added exercices resolutions.

---
+++ 7. bd relacionales/practicas-2-movies/script-twd.sql
@@ -0,0 +1,21 @@
+use movies_db;
+
+drop table if exists TWD;
+
+CREATE TEMPORARY TABLE TWD SELECT e.* 
+FROM episodes e
+INNER JOIN seasons s ON e.season_id = s.id
+INNER JOIN series ss ON ss.id = s.serie_id
+WHERE ss.id = 3;
+
+
+-- season_id 20 -> primer temporada de twd
+SELECT * from TWD WHERE season_id = 20;
+
+-- creación de indice
+
+CREATE INDEX release_date_index on movies(release_date);
+-- creamos este indice para realizar las busquedas por fecha que nos piden el boot
+
+
+
