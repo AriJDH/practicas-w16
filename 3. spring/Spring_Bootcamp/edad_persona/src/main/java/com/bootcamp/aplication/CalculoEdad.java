15151dcb1962698f8a0b1a51d81e1b06e477e680
dpmeli <daniel.patino@mercadolibre.com.co>
2022-08-26 08:26:59 -0500

    Spring: calculo edad

---
+++ 3. spring/Spring_Bootcamp/edad_persona/src/main/java/com/bootcamp/aplication/CalculoEdad.java
@@ -0,0 +1,13 @@
+package com.bootcamp.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoEdad {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoEdad.class, args);
+	}
+
+}
