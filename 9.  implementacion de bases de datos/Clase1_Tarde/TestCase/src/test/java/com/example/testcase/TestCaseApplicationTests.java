e5a10a3a576162763a5c4b6a017bdbf1f287e354
mnicoletta <maximiliano.nicoletta@mercadolibre.com>
2022-08-31 15:17:41 -0300

    Initial commit

---
+++ 9.  implementacion de bases de datos/Clase1_Tarde/TestCase/src/test/java/com/example/testcase/TestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
