f9da92c0df490f355ce51c1a1776784b46c331a3
anmuse <andres.amuse@mercadolibre.com>
2022-08-31 10:46:11 -0300

    h2 db example

---
+++ 9.  implementacion de bases de datos/H2DB/src/main/java/com/example/demo/repository/IMiniSerieRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniSerieRepository extends JpaRepository<MiniSerie, Long> {
+
+}
