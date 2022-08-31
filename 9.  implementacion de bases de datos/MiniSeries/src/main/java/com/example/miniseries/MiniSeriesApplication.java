e3ef73f327102bacfa3f4d98df99d75797144d58
GonzaloNahuelDPMeLi <gonzalo.dipierro@mercadolibre.com>
2022-08-31 12:46:45 -0300

    ADD: Ejercicio > Modulo 18: Implementacion de base de datos > JPA+Hibernate-VIVO

---
+++ 9.  implementacion de bases de datos/MiniSeries/src/main/java/com/example/miniseries/MiniSeriesApplication.java
@@ -0,0 +1,18 @@
+package com.example.miniseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Ejercicio > Modulo 18: Implementacion de base de datos > JPA+Hibernate-VIVO
+ * ESTADO: Finalizado.
+ * @GonzaloNahuelDiPierro
+ * */
+@SpringBootApplication
+public class MiniSeriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniSeriesApplication.class, args);
+    }
+
+}
