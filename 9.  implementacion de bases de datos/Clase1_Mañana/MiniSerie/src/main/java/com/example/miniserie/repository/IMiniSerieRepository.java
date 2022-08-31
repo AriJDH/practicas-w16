8f0f5c6cd21ff41862b42481e30925379f1a551b
mnicoletta <maximiliano.nicoletta@mercadolibre.com>
2022-08-31 10:23:04 -0300

    add Ejercicio MiniSerie

--- 9.  implementacion de bases de datos/Clase1_Mañana/MiniSerie/src/main/java/com/example/miniserie/repository/IMiniSerieRepository.java
+++ 9.  implementacion de bases de datos/Clase1_Mañana/MiniSerie/src/main/java/com/example/miniserie/repository/IMiniSerieRepository.java
@@ -2,6 +2,8 @@ package com.example.miniserie.repository;
 
 import com.example.miniserie.model.MiniSerie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IMiniSerieRepository extends JpaRepository<MiniSerie,Long> {
 }
