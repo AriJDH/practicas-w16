0ca3937ca1c70bfe80cdffffddc2106f13c28240
Ivana Belen Coronel Aguirre <ivana.coronel@mercadolibre.com>
2022-08-30 21:47:54 -0300

    concesionaria-autos

---
+++ 3. spring/spring-concesionariadeautos/src/main/java/com/example/springconcesionariadeautos/entity/Service.java
@@ -0,0 +1,18 @@
+package com.example.springconcesionariadeautos.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Service {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+    private long kilometers;
+    private String description;
+}
