5c495231641f3208273ed9e7beb74e78a3aea12e
mnicoletta <maximiliano.nicoletta@mercadolibre.com>
2022-09-01 12:15:03 -0300

    initial commit

---
+++ 9.  implementacion de bases de datos/Clase2_Mañana/Movie_db/src/main/java/com/example/movie_db/MovieDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDbApplication.class, args);
+    }
+
+}
---
+++ 9.  implementacion de bases de datos/Clase2_Mañana/Movie_db/src/test/java/com/example/movie_db/MovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movie_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
